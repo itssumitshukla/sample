c9fea964279c506e050bab2116962da4aa06c4b3
itssumitshukla <itssumitshukla@gmail.com>
2019-04-15 13:55:52 -0400

    Added jquery logic to remove span

--- To-Do-App/javascript.js
+++ To-Do-App/javascript.js
@@ -2,3 +2,11 @@
 $("li").click(function(){
     $(this).toggleClass("completed")
 })
+
+//click trash icon to delete to-do item
+$("span").click(function(e){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remote();
+    });
+    e.stopPropagation();
+})
