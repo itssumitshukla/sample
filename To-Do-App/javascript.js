8a95d73075183eba2a318631f02a9dbf204d71f3
itssumitshukla <itssumitshukla@gmail.com>
2019-04-15 14:11:31 -0400

    User input properly works

--- To-Do-App/javascript.js
+++ To-Do-App/javascript.js
@@ -1,12 +1,21 @@
 // check off specific todos
-$("li").click(function(){
+$("ul").on("click", "li", function(){
     $(this).toggleClass("completed")
-})
+});
 
 //click trash icon to delete to-do item
-$("span").click(function(e){
+$("ul").on("click", "span", function(e){
     $(this).parent().fadeOut(500, function(){
         $(this).remote();
     });
     e.stopPropagation();
-})
+});
+
+// create new to-do
+$("input[type='text']").keypress(function(e){
+    if(e.which === 13){
+        let toDoText =$(this).val();//get the input from user
+        $("ul").append("<li><span>X</span> " + toDoText + "</li>");//create a new LI
+        $(this).val('');// clear input
+    }
+});
