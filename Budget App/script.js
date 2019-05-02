47da08756ec871d165e68f433a9a2e7fe68cfe3b
itssumitshukla <itssumitshukla@gmail.com>
2019-05-02 00:48:23 -0400

    updated js with event listner

--- Budget App/script.js
+++ Budget App/script.js
@@ -1,18 +1,46 @@
-//budget
+//budget app
+let budgetPatter = (function (params) {
 
-
-var budgetPatter = (function (params) {
-    var x = 23;
-
-    var add = function (a){
-        return x + a;
-    }
-
-    return {
-        publicTest: function(b){
-            console.log(add(b))
-        }
-    }
 })();
 
-console.log(budgetPatter)
+//UI Control
+let UIController = (function () {
+
+})();
+
+// Global app controller
+let controller = (function (budgetCtrl, UICtrl) {
+
+    //control add item
+    let ctrlAddItem = function (){
+
+    };
+
+    //button selector
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem); 
+    {
+        console.log('button was click')
+    };
+
+    // get the input data
+
+    // add iotem to budget calc
+
+    // add the item to the UI
+
+    //Calculate the budget
+
+    // display the budget on the UI
+
+    // keypress for enter === 13
+    document.addEventListener('keypress', function(e){
+        
+        if(e.keyCode=== 13 || event.which === 13){
+            ctrlAddItem();
+        }
+
+    });
+
+})(budgetPatter, UIController);
+
+console.log(budgetPatter);
