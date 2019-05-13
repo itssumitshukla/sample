46430707f1a903149cdcd7858e4e0dbe0b62932e
itssumitshukla <itssumitshukla@gmail.com>
2019-05-13 15:26:21 -0400

    Update site with jquery

--- Omnifood/js/script.js
+++ Omnifood/js/script.js
@@ -2,11 +2,18 @@
 
 $(document).ready(function () {
 
+    // This is for sticky navigation
     $('.js--section--features').waypoint(function(direction){
         if(direction == "down"){
             $('nav').addClass('sticky');
         } else {
             $('nav').removeClass('sticky')
         }
-    } );
+    });
+    // scroll on buttons
+    $('.js--scroll-to-plans').click(function(){
+        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+    });
+    // animations on scroll
+
 });
