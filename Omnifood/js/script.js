63b9350eaa39c2371532fb9f40bd5b9b3f4fcbea
itssumitshukla <itssumitshukla@gmail.com>
2019-05-12 22:28:24 -0400

    Add few jquery features and update main css

---
+++ Omnifood/js/script.js
@@ -0,0 +1,12 @@
+// Testing jquery
+
+$(document).ready(function () {
+
+    $('.js--section--features').waypoint(function(direction){
+        if(direction == "down"){
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky')
+        }
+    } );
+});
