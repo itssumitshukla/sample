c8a2356906df9da20c761ece08bd7e92469a1275
itssumitshukla <itssumitshukla@gmail.com>
2019-11-17 13:52:39 -1000

    Update script and test login

---
+++ Bootstrap Projects/innovationNext/login/script.js
@@ -0,0 +1,17 @@
+// samplelogin
+
+let emailAddress = document.getElementById('inputEmail'),
+  passWord = document.getElementById('inputPassword'),
+  mainButton = document.getElementById('mainButton');
+
+mainButton.addEventListener('click', function (e) {
+  e.preventDefault();
+  if (emailAddress.value === 'test@test.com' && passWord.value === 'test') {
+    window.location.href = 'page2.html';
+  } else if (emailAddress.value === '') {
+    window.location.href = 'errorPage.html';
+  } else {
+    console.log('ERROR')
+  }
+  e.preventDefault();
+}, false);
