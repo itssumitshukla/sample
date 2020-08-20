ea39529f4d710ff5d623e9d2ea3fafb13602f9fc
itssumitshukla <itssumitshukla@gmail.com>
2020-08-19 17:04:53 -1000

    Hard code color box and test js

--- colorPicker/script.js
+++ colorPicker/script.js
@@ -1,5 +1,5 @@
 
-let colors = [
+let colors =  [
 	"rgb(255, 0, 0)",
 	"rgb(255, 255, 0)",
 	"rgb(0, 255, 0)",
@@ -8,10 +8,25 @@ let colors = [
 	"rgb(255, 0, 255)"
 ];
 
-let square = document.querySelectorAll(".square");
+let squares = document.querySelectorAll(".square");
 let pickedColor = colors[3];
 let colorDisplay = document.getElementById("colorDisplay");
+colorDisplay.textContent = pickedColor;
 
-for(let i = 0; i < square.length; i++){
-  square[i].style.backgroundColor = colors[i]
-}
+for(let i = 0; i < squares.length; i++){
+  //add initial colors to square
+  squares[i].style.backgroundColor = colors[i]
+
+  //add click listener to squares
+	squares[i].addEventListener("click", function() {
+		//grab color of clicked squares
+    var clickedColor = this.style.backgroundColor;
+    console.log(clickedColor, pickedColor)
+		//compare color to pickedColor
+		if(clickedColor === pickedColor) {
+			alert("Correct!");
+		} else {
+			alert("WRONG!!!");
+		}
+	});
+};
