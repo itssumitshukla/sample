2c9625b1822666d8d9ca78dbb9aec181a8347fc2
itssumitshukla <itssumitshukla@gmail.com>
2020-10-05 22:54:22 -1000

    setup fetch parameter

---
+++ AsyncAwaitFetch/Fetch/script.js
@@ -0,0 +1,7 @@
+const button = document.querySelector('button');
+const divDiv = document.getElementById('here');
+let url = 'https://swapi.dev/api/people/1/';
+
+button.addEventListener('click', function () {
+
+});
