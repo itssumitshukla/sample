01c2b23c934693bbde548a4fc5e63136ea5ae686
itssumitshukla <itssumitshukla@gmail.com>
2020-10-05 23:04:31 -1000

    Call people endpoint and loop over names.

--- AsyncAwaitFetch/Fetch/script.js
+++ AsyncAwaitFetch/Fetch/script.js
@@ -1,7 +1,16 @@
 const button = document.querySelector('button');
 const divDiv = document.getElementById('here');
-let url = 'https://swapi.dev/api/people/1/';
+let url = 'https://swapi.dev/api/people/';
 
 button.addEventListener('click', function () {
-
+  fetch(url)
+    .then(response => response.json()) // convert to json
+    .then(data => {
+      for (let ii of data.results) {
+        divDiv.innerHTML += ii.name;
+        console.log(ii.name)
+      }
+      //divDiv.style.display = 'block';
+    }) //print data to console
+    .catch(err => console.log('Request Failed', err)); // Catch errors
 });
