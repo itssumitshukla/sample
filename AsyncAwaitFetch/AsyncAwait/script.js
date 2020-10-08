2ac7bbbe5dc885d6d2f38e34e15006c86501c39d
itssumitshukla <itssumitshukla@gmail.com>
2020-10-07 19:36:41 -1000

    Test axios and set up async and await

--- AsyncAwaitFetch/AsyncAwait/script.js
+++ AsyncAwaitFetch/AsyncAwait/script.js
@@ -1,6 +1,17 @@
 let url = 'https://swapi.dev/api/people/1/';
 
-let data = axios.get(url);
+// check axios
+// function getPeople() {
+//   let data = axios.get(url);
+//   return data;
+// }
+// getPeople().then((res) => {
+//   console.log(res.data);
+// });
 
+async function getPeople() {
+  let res = await axios.get(url);
+  console.log(res.data)
+}
 
-console.log(data);
+getPeople();
