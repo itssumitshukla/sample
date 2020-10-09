56ecb3c9e104f0989538606378839b01708ee393
itssumitshukla <itssumitshukla@gmail.com>
2020-10-08 17:50:49 -1000

    Set up parallel async requests

--- AsyncAwaitFetch/AsyncAwait/script.js
+++ AsyncAwaitFetch/AsyncAwait/script.js
@@ -22,26 +22,28 @@ let url3 = 'https://swapi.dev/api/people/3/';
 // getPeople();
 
 //Testing Sequential Reuests
+// async function getPeople() {
+//   let res1 = await axios.get(url1);
+//   let res2 = await axios.get(url2);
+//   let res3 = await axios.get(url3);
+//   console.log(res1.data.name)
+//   console.log(res2.data.name)
+//   console.log(res3.data.name)
+// }
+// getPeople();
+
+
+//Testing parallel request
 async function getPeople() {
   let res1 = await axios.get(url1);
   let res2 = await axios.get(url2);
   let res3 = await axios.get(url3);
-  console.log(res1.data.name)
-  console.log(res2.data.name)
-  console.log(res3.data.name)
+  let tt1 = await res1;
+  let tt2 = await res2;
+  let tt3 = await res3;
+  console.log(tt1.data.name)
+  console.log(tt2.data.name)
+  console.log(tt3.data.name)
 }
 
 getPeople();
-
-
-//Testing parallel request
-// async function getPeople() {
-//   try {
-//     let res = await axios.get(url1);
-//     console.log(res.data)
-//   } catch (e) {
-//     console.log('IN CATCH: ', e)
-//   }
-// }
-
-// getPeople();
