8dc6e4906ba6c39c547157848fc866dcfbf6d51c
itssumitshukla <itssumitshukla@gmail.com>
2020-10-09 23:19:33 -1000

    Chaining multiple requests

--- AsyncAwaitFetch/XHRrequest/script.js
+++ AsyncAwaitFetch/XHRrequest/script.js
@@ -3,6 +3,21 @@ let firstReq = new XMLHttpRequest();
 firstReq.addEventListener('load', function(){
   console.log('IT WORKED')
   let dataBack = JSON.parse(this.responseText);
+  //Chaining requests
+  let filmUrl = dataBack.results[0].films[0];
+  let filmReq = new XMLHttpRequest();
+  filmReq.addEventListener('load', function(){
+    console.log(this)
+  });
+
+  filmReq.addEventListener('error', ()=>{
+    console.log('ERROR!!!!!')
+  });
+
+  //Looping over the result and getting just the name of the planets
+  // for(let tt of dataBack.results){
+  //   console.log(tt.name);
+  // }
   console.log(dataBack)
 });
 
@@ -11,6 +26,6 @@ firstReq.addEventListener('error', ()=>{
   console.log('ERROR!!!!!')
 });
 
-firstReq.open('GET', 'https://swapi.dev/api/planets/1/');
+firstReq.open('GET', 'https://swapi.dev/api/planets/');
 firstReq.send();
 // console.log(firstReq)
