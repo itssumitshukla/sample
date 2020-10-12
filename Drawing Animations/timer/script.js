cde100b6854c66a5b3235cf14ccb685dc8cd946e
itssumitshukla <itssumitshukla@gmail.com>
2020-10-11 21:15:36 -1000

    setup intervals for start and pause

--- Drawing Animations/timer/script.js
+++ Drawing Animations/timer/script.js
@@ -7,10 +7,22 @@ class Timer{
 
     //setting up event listner
     this.startButton.addEventListener('click', this.start);
+    this.pauseButton.addEventListener('click', this.pause);
+
   }
 
-  start(){
-    console.log('You started')
+  start = () => {
+    this.tick();
+    this.interval = setInterval(this.tick, 1000);
+    
+  };
+
+  pause = () =>{
+    clearInterval(this.interval);
+  }
+
+  tick = ()=>{
+    console.log('TICK')
   }
 }
 
