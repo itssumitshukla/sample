7c157c2c93a6a8d6f1474834cf3c8061955648de
itssumitshukla <itssumitshukla@gmail.com>
2020-10-12 11:27:28 -1000

    test callbacks

--- Drawing Animations/timer/script.js
+++ Drawing Animations/timer/script.js
@@ -2,10 +2,16 @@
 
 //setup timer
 class Timer{
-  constructor(durationInput, startButton, pauseButton){
+  constructor(durationInput, startButton, pauseButton, callbacks){
     this.durationInput =  durationInput;
     this.startButton =  startButton;
     this.pauseButton = pauseButton;
+
+    if(callbacks){
+      this.onStart = callbacks.onStart;
+      this.onComplete = callbacks.onComplete;
+      this.onTick = callbacks.onTick;
+    }
 
     //setting up event listner
     this.startButton.addEventListener('click', this.start);
@@ -14,6 +20,9 @@ class Timer{
   }
 
   start = () => {
+    if(this.onStart){
+      this.onStart();
+    }
     this.tick();
     this.interval = setInterval(this.tick, 1000);
     
@@ -25,11 +34,16 @@ class Timer{
 
   tick = () => {
     if(this.timeRemaining <= 0){
-      this.pause()
+      this.pause();
+      if(this.onComplete){
+        this.onComplete();
+      }
     } else {
       this.timeRemaining = this.timeRemaining - 1;
+      if(this.onTick) {
+        this.onTick();
+      }
     };
-    this.timeRemaining = this.timeRemaining-1;
   };
 
   get timeRemaining(){
@@ -46,4 +60,14 @@ let durationInput = document.querySelector("#duration");
 let startButton = document.querySelector("#start");
 let pauseButton = document.querySelector("#pause");
 
-const timer = new Timer(durationInput, startButton, pauseButton);
+const timer = new Timer(durationInput, startButton, pauseButton, {
+  onStart(){
+    console.log('Timer started');
+  },
+  onTick(){
+    console.log('Timer just ticked down');
+  },
+  onComplete(){
+    console.log('Timer is completed');
+  }
+});
