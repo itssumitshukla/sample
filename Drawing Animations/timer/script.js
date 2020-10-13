af2da86c6d87cbe1f1a52389aae1ad82459311d4
itssumitshukla <itssumitshukla@gmail.com>
2020-10-12 19:16:45 -1000

    set up animation in dom and test callback

--- Drawing Animations/timer/script.js
+++ Drawing Animations/timer/script.js
@@ -3,13 +3,20 @@
 let durationInput = document.querySelector("#duration");
 let startButton = document.querySelector("#start");
 let pauseButton = document.querySelector("#pause");
+let circle = document.querySelector('circle');
+
+let perimeter = circle.getAttribute('r') * 2 * Math.PI;
+circle.setAttribute('stroke-dasharray', perimeter);
+
+let currentOffset = 0;
 
 const timer = new Timer(durationInput, startButton, pauseButton, {
   onStart(){
     console.log('Timer started');
   },
   onTick(){
-    console.log('Timer just ticked down');
+    circle.setAttribute('stroke-dashoffset', currentOffset);
+    currentOffset = currentOffset - 50;
   },
   onComplete(){
     console.log('Timer is completed');
