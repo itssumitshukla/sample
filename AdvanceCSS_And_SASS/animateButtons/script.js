dd4d74702a272f07ec74c59107a36a2d7a4926ea
itssumitshukla <itssumitshukla@gmail.com>
2020-10-19 00:12:42 -1000

    setup toggle

--- AdvanceCSS_And_SASS/animateButtons/script.js
+++ AdvanceCSS_And_SASS/animateButtons/script.js
@@ -1,8 +1,13 @@
 
 let btn = document.getElementById('btn');
 let cards = document.getElementById('here');
+let yay = 'TEST';
 
 btn.addEventListener('click', function(){
-  cards.classList.add("d-block");
+  cards.classList.toggle("d-block");
+//   cards = `<div class="card-body">
+//   <h5 class="card-title">${yay}</h5>
+//   <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+// </div>`
   console.log('YAY')
 })
