442b7f3885bbded1d18e5b9ad3a14044fb0d4927
itssumitshukla <itssumitshukla@gmail.com>
2020-10-19 00:25:58 -1000

    Set up list in one card using fetch

--- AdvanceCSS_And_SASS/animateButtons/script.js
+++ AdvanceCSS_And_SASS/animateButtons/script.js
@@ -1,13 +1,34 @@
-
 let btn = document.getElementById('btn');
 let cards = document.getElementById('here');
-let yay = 'TEST';
+let url = 'https://swapi.dev/api/people/';
 
-btn.addEventListener('click', function(){
-  cards.classList.toggle("d-block");
-//   cards = `<div class="card-body">
-//   <h5 class="card-title">${yay}</h5>
-//   <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-// </div>`
-  console.log('YAY')
-})
+// btn.addEventListener('click', function() {
+//       cards.classList.toggle("d-block");
+//       fetch(url)
+//         .then(response => response.json()) // convert to json
+//         .then(data => {
+//           for (let ii of data.results) {
+//             cards.style.display = 'block';
+//             // cards.innerHTML += `<strong>${ii.name}</strong> was born in ${ii.birth_year}! <br>`;
+//             cards.innerHTML += `<h5 class="card-title">${ii.name}</h5>
+//             <p class="card-text">born in ${ii.birth_year}! <br> </p>`;
+//           }}) //print data to console
+//          .catch(err => console.log('Request Failed', err)); // Catch errors
+//           // });
+//         });
+
+//trying whole div
+btn.addEventListener('click', function() {
+      cards.classList.toggle("d-block");
+      fetch(url)
+        .then(response => response.json()) // convert to json
+        .then(data => {
+          for (let ii of data.results) {
+            cards.style.display = 'block';
+            // cards.innerHTML += `<strong>${ii.name}</strong> was born in ${ii.birth_year}! <br>`;
+            cards.innerHTML += `<h5 class="card-title">${ii.name}</h5>
+            <p class="card-text">born in ${ii.birth_year}! <br> </p>`;
+          }}) //print data to console
+         .catch(err => console.log('Request Failed', err)); // Catch errors
+          // });
+        });
