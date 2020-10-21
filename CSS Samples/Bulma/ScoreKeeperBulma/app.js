8097f003eeebd58d7811b6606157739ad434f16d
itssumitshukla <itssumitshukla@gmail.com>
2020-10-20 20:06:41 -1000

    Set up dom and test js

---
+++ CSS Samples/Bulma/ScoreKeeperBulma/app.js
@@ -0,0 +1,10 @@
+const p1 = {
+  score: 0,
+  button: document.querySelector('#p1Button'),
+  display: document.querySelector('#p1Display')
+}
+const p2 = {
+  score: 0,
+  button: document.querySelector('#p2Button'),
+  display: document.querySelector('#p2Display')
+}
