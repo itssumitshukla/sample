44897f8f10e6a970e015a47e2464ac1ad6b9b6d9
itssumitshukla <itssumitshukla@gmail.com>
2020-10-21 18:01:57 -1000

    test promises and append result/images back to dom

--- javascriptProjects/AsyncAwaitFetch/Fetch/MovieDB/app.js
+++ javascriptProjects/AsyncAwaitFetch/Fetch/MovieDB/app.js
@@ -1,9 +1,18 @@
-
-let form  = document.querySelector('#searchForm');
-form.addEventListener('submit', function(e){
+let form = document.querySelector('#searchForm');
+form.addEventListener('submit', async function (e) {
   e.preventDefault();
   let searchTerm = form.elements.query.value;
-  
-  console.dir(this)
-})
+  let res = await axios.get(`http://api.tvmaze.com/search/shows?q=${searchTerm}`);
+  makeImages(res.data);
+});
 
+
+let makeImages = (shows) => {
+  for (let result of shows) {
+    if (result.show.image) {
+      let img = document.createElement('IMG');
+      img.src = result.show.image.medium;
+      document.body.append(img);
+    }
+  }
+}
