dd5d9484a6fa51ec1b80c90fe08263337dd349fe
itssumitshukla <itssumitshukla@gmail.com>
2020-10-22 23:50:35 -1000

    set up 2 event listeners and test appending to dom

--- javascriptProjects/formTest/script.js
+++ javascriptProjects/formTest/script.js
@@ -1,11 +1,23 @@
+
 const forma = document.querySelector("#username");
 let h1g =  document.querySelector('h1');
+let btn = document.querySelector('#btn');
 
 forma.addEventListener('input', function (e) {
   if(forma.value === ''){
-     h1g.innerText = 'Enter Your Username'
+     h1g.innerText = 'Enter some Words to add'
   } else {
-     h1g.innerText = `Welcome, ${forma.value}`
-  }
-   
+     h1g.innerText = `Welcome, you will be adding this to the list.... ${forma.value}`
+  } 
 });
+
+btn.addEventListener('click', function (e) {
+  e.preventDefault();
+let lastCh = document.createElement('P');
+lastCh.innerText = forma.value;
+document.body.append(lastCh);
+console.log(`${forma.value}`)
+forma.value = '';
+});
+
+console.log(`${forma.value}`)
