c5aa9ad0224ff39b4c7d833dbea73e66d3ec06a1
itssumitshukla <itssumitshukla@gmail.com>
2020-12-18 18:02:40 -1000

    refactor code

--- javascriptProjects/Infinity Scroll/script.js
+++ javascriptProjects/Infinity Scroll/script.js
@@ -6,19 +6,30 @@ const count = 10;
 const apiKey = API_KEY;
 const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+//helper function
+function setAttributes(element, attributes) {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+}
+
 
 //create elements for links and photos and add it to dom
 function displayPhotos(){
   photosArray.forEach((photo)=>{
     //Create ancho to unsplash
     const item = document.createElement('a');
-    item.setAttribute('href', photo.links.html);
-    item.setAttribute('target', "_blank");
-    //create imate
+    setAttributes(item, {
+      href: photo.links.html,
+      target: '_blank',
+    });
+    //create image
     const img = document.createElement('img');
-    img.setAttribute('src', photo.urls.regular);
-    img.setAttribute('alt', photo.alt_description);
-    img.setAttribute('title', photo.alt_description);
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
     //put image in anchor and in image element
     item.appendChild(img);
     imageContainer.appendChild(item);
