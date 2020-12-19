eb945d68c8571218ee9edd9b3568fcbb6ec810b3
itssumitshukla <itssumitshukla@gmail.com>
2020-12-18 17:53:16 -1000

    fixed image issue with api

--- javascriptProjects/Infinity Scroll/script.js
+++ javascriptProjects/Infinity Scroll/script.js
@@ -6,19 +6,22 @@ const count = 10;
 const apiKey = API_KEY;
 const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+
 //create elements for links and photos and add it to dom
 function displayPhotos(){
   photosArray.forEach((photo)=>{
     //Create ancho to unsplash
-    let item = document.createElement('a');
+    const item = document.createElement('a');
     item.setAttribute('href', photo.links.html);
     item.setAttribute('target', "_blank");
     //create imate
-    let img = document.createElement('img');
-    img.setAttribute('src', 'photo.urls.regular');
-    img.setAttribute('alt', 'photo.alt_description');
-    img.setAttribute('title', 'photo.alt_description');
-
+    const img = document.createElement('img');
+    img.setAttribute('src', photo.urls.regular);
+    img.setAttribute('alt', photo.alt_description);
+    img.setAttribute('title', photo.alt_description);
+    //put image in anchor and in image element
+    item.appendChild(img);
+    imageContainer.appendChild(item);
    });
 }
 
