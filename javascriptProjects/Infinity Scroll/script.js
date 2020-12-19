f70bd8d690128eba7f77de86dbbb03202e687b7f
itssumitshukla <itssumitshukla@gmail.com>
2020-12-18 17:42:06 -1000

    set up functions and make sure api works

--- javascriptProjects/Infinity Scroll/script.js
+++ javascriptProjects/Infinity Scroll/script.js
@@ -1,16 +1,33 @@
 
-//https://api.unsplash.com/photos/?client_id=YOUR_ACCESS_KEY
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
+let photosArray = [];
 const count = 10;
-//process.env.API_KEY; 
 const apiKey = API_KEY;
 const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+//create elements for links and photos and add it to dom
+function displayPhotos(){
+  photosArray.forEach((photo)=>{
+    //Create ancho to unsplash
+    let item = document.createElement('a');
+    item.setAttribute('href', photo.links.html);
+    item.setAttribute('target', "_blank");
+    //create imate
+    let img = document.createElement('img');
+    img.setAttribute('src', 'photo.urls.regular');
+    img.setAttribute('alt', 'photo.alt_description');
+    img.setAttribute('title', 'photo.alt_description');
+
+   });
+}
 
 //Get photos from Unslpash API 
 async function getPhotos(){
   try {
     const response = await fetch(apiURL);
-    const data = await response.json();
-    console.log(data);
+    photosArray= await response.json();
+    displayPhotos();
   } catch (err) {
     //Catch error
   }
