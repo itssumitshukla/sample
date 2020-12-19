8e69b97bceab10a709711a4ddfdf195e1557c0e1
itssumitshukla <itssumitshukla@gmail.com>
2020-12-18 15:41:15 -1000

    Testing api key and hide script

--- javascriptProjects/Infinity Scroll/script.js
+++ javascriptProjects/Infinity Scroll/script.js
@@ -1,10 +1,19 @@
 
-//Require
-require('dotenv').config();
-
 //https://api.unsplash.com/photos/?client_id=YOUR_ACCESS_KEY
 const count = 10;
-const apiKey = process.env.API_KEY;
+//process.env.API_KEY; 
+const apiKey = API_KEY;
 const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
 //Get photos from Unslpash API 
+async function getPhotos(){
+  try {
+    const response = await fetch(apiURL);
+    const data = await response.json();
+    console.log(data);
+  } catch (err) {
+    //Catch error
+  }
+}
+
+getPhotos();
