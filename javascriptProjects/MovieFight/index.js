c8417375cc5c92c6eb237693c4cdeca897444fac
itssumitshukla <itssumitshukla@gmail.com>
2020-12-26 18:56:34 -1000

    Set up axious and test api

--- javascriptProjects/MovieFight/index.js
+++ javascriptProjects/MovieFight/index.js
@@ -1,1 +1,13 @@
-console.log('Hi there!');
+//API
+const fetchData = async() =>{
+  const response = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apikey: 'ec3a8c4d',
+      s: 'avengers'
+    }
+  });
+
+  console.log(response.data);
+};
+
+fetchData();
