651bd36fb9183109b08336d24fa8750c6d2d53af
itssumitshukla <itssumitshukla@gmail.com>
2020-12-27 00:36:06 -1000

    Moved divs from html and made reusable code in js

--- javascriptProjects/MovieFight/index.js
+++ javascriptProjects/MovieFight/index.js
@@ -1,5 +1,5 @@
 //API
-const fetchData = async(searchTerm) =>{
+const fetchData = async (searchTerm) => {
   const response = await axios.get('http://www.omdbapi.com/', {
     params: {
       apikey: 'ec3a8c4d',
@@ -7,19 +7,33 @@ const fetchData = async(searchTerm) =>{
     }
   });
 
-  if(response.data.Error){
+  if (response.data.Error) {
     return [];
   }
 
   return response.data.Search;
 };
 
+const root = document.querySelector('.autocomplete');
+root.innerHTML = `
+  <label><b>Search for a movie</b></label>
+  <input class="input"/>
+    <div class="dropdown">
+      <div class="dropdown-menu">
+        <div class="dropdown-content results">
+        </div>
+      </div>
+    </div>
+`;
+
 const input = document.querySelector('input');
+const dropdown = document.querySelector('.dropdown');
+const resultWrapper = document.querySelector('.results');
 
 
 const onInput = async (event) => {
   const movies = await fetchData(event.target.value);
-  for(let movie of movies){
+  for (let movie of movies) {
     let div = document.createElement('div');
     div.innerHTML = `
     <img src ="${movie.Poster}"  />
