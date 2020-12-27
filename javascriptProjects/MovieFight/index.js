d01b5e8e8acedc3cd6363406316249c3736fd47f
itssumitshukla <itssumitshukla@gmail.com>
2020-12-26 22:52:16 -1000

    test input and serach function

--- javascriptProjects/MovieFight/index.js
+++ javascriptProjects/MovieFight/index.js
@@ -1,13 +1,17 @@
 //API
-const fetchData = async() =>{
+const fetchData = async(searchTerm) =>{
   const response = await axios.get('http://www.omdbapi.com/', {
     params: {
       apikey: 'ec3a8c4d',
-      s: 'avengers'
+      s: searchTerm,
     }
   });
 
   console.log(response.data);
+  console.log(response.data.Plot);
 };
 
-fetchData();
+const input = document.querySelector('input');
+input.addEventListener('input', (event)=>{
+  fetchData(event.target.value);
+});
