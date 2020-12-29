5a5694c0694ec79ba5625f6948b2bc2e1480b62d
itssumitshukla <itssumitshukla@gmail.com>
2020-12-28 16:20:29 -1000

    set up divs

--- javascriptProjects/MovieFight/index.js
+++ javascriptProjects/MovieFight/index.js
@@ -100,5 +100,25 @@ const movieTemplate = (movieDetail) => {
     </div>
   </div>
 </article>
+<article class="notification is-primary">
+  <p class="title">${movieDetail.Awards}</p>
+  <p class="subtitle">Awards</p>
+</article>
+<article class="notification is-primary">
+  <p class="title">${movieDetail.BoxOffice}</p>
+  <p class="subtitle">BoxOffice</p>
+</article>
+<article class="notification is-primary">
+  <p class="title">${movieDetail.Metascore}</p>
+  <p class="subtitle">Metascore</p>
+</article>
+<article class="notification is-primary">
+  <p class="title">${movieDetail.imdbRating}</p>
+  <p class="subtitle">imdbRating</p>
+</article>
+<article class="notification is-primary">
+  <p class="title">${movieDetail.imdbVotes}</p>
+  <p class="subtitle">imdbVotes</p>
+</article>
   `;
 };
