bee55f4220f5af1706f8292d42c793d98e778751
itssumitshukla <itssumitshukla@gmail.com>
2021-01-11 19:17:45 -1000

    create a rectangle

--- javascriptProjects/maze/index.js
+++ javascriptProjects/maze/index.js
@@ -8,15 +8,22 @@ const {
 } = Matter;
 
 const engine = Engine.create();
-const { world } = engine;
+const {
+  world
+} = engine;
 const render = Render.create({
   element: document.body,
   engine: engine,
   options: {
     width: 800,
-    height:600
+    height: 600
   }
 });
 
 Render.run(render);
-Runner.run(Runner.create(),engine);
+Runner.run(Runner.create(), engine);
+
+const shape = Bodies.rectangle(200, 200, 50, 50, {
+  isStatic: true
+});
+World.add(world, shape);
