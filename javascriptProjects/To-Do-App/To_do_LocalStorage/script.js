6ad4944892c1ce1f634c5028f7873210b263a95c
itssumitshukla <itssumitshukla@gmail.com>
2021-01-25 19:35:46 -1000

    setup form event handler

---
+++ javascriptProjects/To-Do-App/To_do_LocalStorage/script.js
@@ -0,0 +1,35 @@
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+//Load all event lsiteners
+loadEventListeners();
+
+//Load all event listenrs
+function loadEventListeners() {
+  form.addEventListener('submit', addTask);
+}
+
+// //Add task
+function addTask(e) {
+  if(taskInput.value ===''){
+    alert('ADD A TASK')
+  }
+
+  //create li element
+  const li = document.createElement('li');
+  //add Class
+  li.className = 'collection-item';
+  li.appendChild(document.createTextNode(taskInput.value));
+  const link = document.createElement('a');
+  link.className = 'delete-item secondary-content';
+  link.innerHTML = '<i class="fas fa-times" ></i>'
+  li.appendChild(link);
+  taskList.appendChild(li);
+
+  taskInput.value='';
+
+  e.preventDefault();
+};
