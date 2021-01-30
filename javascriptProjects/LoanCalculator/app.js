799647af7c2b7bb55c3e9f350127f658dc8893e3
itssumitshukla <itssumitshukla@gmail.com>
2021-01-29 20:39:06 -1000

    setup calculation

--- javascriptProjects/LoanCalculator/app.js
+++ javascriptProjects/LoanCalculator/app.js
@@ -8,6 +8,24 @@ function calculateResults(e) {
   let years = document.getElementById('years');
   let monthlyPayment = document.getElementById('monthly-payment');
   let totalPayment = document.getElementById('total-payment');
+  let totalInterest = document.getElementById('total-interest');
 
+  let principal = parseFloat(amount.value);
+  let calculatedInterest = parseFloat(interest.value)/ 100 / 12;
+  let calculatedPayments = parseFloat(years.value) * 12;
+
+  //compute monthly payments
+  let x = Math.pow( 1 + calculatedInterest, calculatedPayments);
+  let monthly = (principal * x * calculatedInterest)/(x-1);
+
+  if(isFinite(monthly)){
+    monthlyPayment.value = monthly.toFixed(2);
+    totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+    totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+  } else {
+    console.log('yooo')
+  }
+
+  console.log(principal)
   e.preventDefault();
 };
