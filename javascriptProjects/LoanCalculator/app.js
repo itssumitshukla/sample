9982d17ecac98505eacddc792592915dc86ab162
itssumitshukla <itssumitshukla@gmail.com>
2021-01-29 20:44:49 -1000

    setup error and clear error function

--- javascriptProjects/LoanCalculator/app.js
+++ javascriptProjects/LoanCalculator/app.js
@@ -23,9 +23,24 @@ function calculateResults(e) {
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
   } else {
-    console.log('yooo')
+    showError('Please Check your numbers');
   }
-
-  console.log(principal)
   e.preventDefault();
 };
+
+//Shor error function
+
+function showError(error) {
+  let errorDiv = document.createElement('div');
+  let card = document.querySelector('.card');
+  let heading = document.querySelector('.heading');
+  errorDiv.className = 'alert alert-danger';
+  errorDiv.appendChild(document.createTextNode(error));
+  card.insertBefore(errorDiv, heading);
+  //Clear error
+  setTimeout(clearError, 3000);
+};
+
+function clearError() {
+  document.querySelector('.alert').remove();
+}
