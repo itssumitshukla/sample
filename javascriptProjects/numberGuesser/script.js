983aa93cbc9fa5f71b3f220961d46782851fb3ec
itssumitshukla <itssumitshukla@gmail.com>
2021-01-31 21:47:49 -1000

    Set up set message and winning function

--- javascriptProjects/numberGuesser/script.js
+++ javascriptProjects/numberGuesser/script.js
@@ -11,3 +11,31 @@ let game = document.querySelector('#game'),
     guessBtn = document.querySelector('#guess-btn'),
     guessInput = document.querySelector('#guess-input'),
     message = document.querySelector('.message');
+
+//Assign UI min and Max
+minNum.textContent = min;
+maxNum.textContent = max;
+//event listner
+guessBtn.addEventListener('click', function(){
+  let guess = parseInt(guessInput.value);
+  //Validate
+  if(isNaN(guess) || guess < min || guess > max){
+    setMessage(`Please enter a number between ${min} and ${max}`, 'red');
+  }
+  //Check if won
+  if(guess === winningNum){
+    //Disable input
+    guessInput.disabled = true;
+    guessInput.style.borderColor = 'green';
+    //Set message
+    setMessage(`${winningNum} is correct, YOU WIN!`, 'green');
+  } else {
+
+  }
+});
+
+//Set Message Function
+function setMessage(msg, color){
+  message.style.color = color;
+  message.textContent = msg;
+};
