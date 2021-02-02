914a44e745ea0627b85c89cb4481126df88b6d29
itssumitshukla <itssumitshukla@gmail.com>
2021-02-01 20:48:59 -1000

    setup function for when user inputs wrong number

--- javascriptProjects/numberGuesser/script.js
+++ javascriptProjects/numberGuesser/script.js
@@ -1,41 +1,53 @@
 // Game Values
 let min = 1,
-    max = 10,
-    winningNum = 2,
-    guessesLeft = 3;
+  max = 10,
+  winningNum = 2,
+  guessesLeft = 3;
 
 // UI Elements
 let game = document.querySelector('#game'),
-    minNum = document.querySelector('.min-num'),
-    maxNum = document.querySelector('.max-num'),
-    guessBtn = document.querySelector('#guess-btn'),
-    guessInput = document.querySelector('#guess-input'),
-    message = document.querySelector('.message');
+  minNum = document.querySelector('.min-num'),
+  maxNum = document.querySelector('.max-num'),
+  guessBtn = document.querySelector('#guess-btn'),
+  guessInput = document.querySelector('#guess-input'),
+  message = document.querySelector('.message');
 
 //Assign UI min and Max
 minNum.textContent = min;
 maxNum.textContent = max;
 //event listner
-guessBtn.addEventListener('click', function(){
+guessBtn.addEventListener('click', function () {
   let guess = parseInt(guessInput.value);
   //Validate
-  if(isNaN(guess) || guess < min || guess > max){
+  if (isNaN(guess) || guess < min || guess > max) {
     setMessage(`Please enter a number between ${min} and ${max}`, 'red');
   }
   //Check if won
-  if(guess === winningNum){
+  if (guess === winningNum) {
+    //Game over --Won
     //Disable input
     guessInput.disabled = true;
     guessInput.style.borderColor = 'green';
     //Set message
     setMessage(`${winningNum} is correct, YOU WIN!`, 'green');
   } else {
-
+    //Wrong Number
+    guessesLeft -= 1;
+    if (guessesLeft === 0) {
+      //Game over --Lost
+      //Disable input
+      guessInput.disabled = true;
+      guessInput.style.borderColor = 'red';
+      //Set message
+      setMessage(`Game Over, you lost! the correct number was ${winningNum}`, 'green');
+    } else {
+      //Game continues-- answer wrong
+    }
   }
 });
 
 //Set Message Function
-function setMessage(msg, color){
+function setMessage(msg, color) {
   message.style.color = color;
   message.textContent = msg;
 };
