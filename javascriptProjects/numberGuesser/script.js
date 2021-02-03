ecf1dcf93be5c2deb7becbc48d4d6d1b09240443
itssumitshukla <itssumitshukla@gmail.com>
2021-02-02 21:23:30 -1000

    setup play again button

--- javascriptProjects/numberGuesser/script.js
+++ javascriptProjects/numberGuesser/script.js
@@ -52,6 +52,10 @@ function gameOver(won, msg) {
   message.style.color = color;
   //Set message
   setMessage(msg);
+
+  //Play again
+  guessBtn.value = 'Play Again';
+  guessBtn.className += 'play-again';
 };
 
 //Set Message Function
