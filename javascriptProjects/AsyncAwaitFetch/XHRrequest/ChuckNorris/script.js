f2ec93748a5981ddac5bc05a9039c7ea4575560e
itssumitshukla <itssumitshukla@gmail.com>
2021-02-08 13:43:26 -1000

    fixed json parse issue

--- javascriptProjects/AsyncAwaitFetch/XHRrequest/ChuckNorris/script.js
+++ javascriptProjects/AsyncAwaitFetch/XHRrequest/ChuckNorris/script.js
@@ -2,22 +2,38 @@ let button = document.querySelector('.get-jokes');
 
 button.addEventListener('click', getJokes);
 
-function getJokes(e){
+function getJokes(e) {
   let number = document.querySelector('input[type="number"]').value;
 
   //XHR
   let xhr = new XMLHttpRequest();
-  xhr.open('GET',`http://api.icndb.com/jokes/random/${number.value}`, true);
-  
-  xhr.onload = function(){
-    if(this.status === 200) {
-      let response = this.responseText;
-      console.log(response);
+  xhr.open('GET', `http://api.icndb.com/jokes/random/${number.value}`, true);
+
+  xhr.onload = function () {
+    if (this.status === 200) {
+      let response = JSON.parse(this.responseText);
+      let output = '';
+      if (response.type === 'success') {
+
+        // Check if Array 
+        if (Array.isArray(response.value)) {
+          // Is Array
+          response.value.forEach(function (joke) {
+            output += `<li>${joke.joke}</li>`;
+          });
+        } else if(response.value.hasOwnProperty('joke')){
+            // Is an object and has a property of joke.
+            output += `<li>${response.value.joke}</li>`;
+        } else {
+          // something bad happened.
+            output += '<li>Something Went wrong>/li>';
+        }
+      }
+      document.querySelector('.jokes').innerHTML = output;
     }
   }
 
   xhr.send();
-  
-  console.log('Yo' + number);
+  number.value = '';
   e.preventDefault();
 }
