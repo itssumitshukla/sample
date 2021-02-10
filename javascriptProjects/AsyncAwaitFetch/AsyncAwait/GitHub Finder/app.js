4c64856774fa327130fb60c77183bfa8e967c793
itssumitshukla <itssumitshukla@gmail.com>
2021-02-09 21:25:17 -1000

    fixed  github profile error and dynamic search

--- javascriptProjects/AsyncAwaitFetch/AsyncAwait/GitHub Finder/app.js
+++ javascriptProjects/AsyncAwaitFetch/AsyncAwait/GitHub Finder/app.js
@@ -1,18 +1,27 @@
-//Init Github
-let gitHub = new GitHub;
-
-//Search input
-let searchUser = document.getElementById('searchUser');
-
-searchUser.addEventListener('keyup', (e)=>{
-  //getinput text
-  let userText = e.target.value;
-
-  if(userText !== ''){
-    //make Hhtp call
-    gitHub.getUser(userText)
-      .then(data => {
-        console.log(data);
-      })
-  }
-})
+// Init github
+const github = new GitHub();
+ 
+// search Input 
+const searchUser = document.getElementById("searchUser");
+ 
+// Search input event Listener
+searchUser.addEventListener("keyup", (e) => {
+    // get Input value
+    const userText = e.target.value
+ 
+    if (userText !== "") {
+        // Make http call
+        github.getUser(userText)
+            .then(data => {
+              if(data.profile.message === 'Not Found'){
+                //Show Alert
+              } else {
+                //Show profile
+              }
+                console.log(data);
+            })
+    } else {
+      //Clear profile
+      
+    }
+});  
--- javascriptProjects/AsyncAwaitFetch/AsyncAwait/GitHub Finder/github.js
+++ javascriptProjects/AsyncAwaitFetch/AsyncAwait/GitHub Finder/github.js
@@ -1,14 +1,14 @@
 //github
 class GitHub {
-  constructor(){
-    this.client_id = 'd4d0961c14b7006eab87';
-    this.client_secret='9e55192e5d00f567132708c3e0ab5eb773fc963f';
+  constructor() {
+    this.client_id = "d4d0961c14b7006eab87";
+    this.client_secret = "9e55192e5d00f567132708c3e0ab5eb773fc963f";
   }
 
-  async getUser(user){
-    let profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
-    let profileData = await profileResponse.json();
+    const profile = await profileResponse.json();
 
     return {
       profile
