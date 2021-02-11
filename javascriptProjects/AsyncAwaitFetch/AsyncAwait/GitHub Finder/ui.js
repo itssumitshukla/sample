aa7d2138069242886e61339dfbeb1d891a11a227
itssumitshukla <itssumitshukla@gmail.com>
2021-02-10 23:33:00 -1000

    fix dom repo issue

--- javascriptProjects/AsyncAwaitFetch/AsyncAwait/GitHub Finder/ui.js
+++ javascriptProjects/AsyncAwaitFetch/AsyncAwait/GitHub Finder/ui.js
@@ -33,8 +33,26 @@ class UI {
     `;
   }
   //Show repos
-  showRepos(repos){
+  showRepos(repos) {
     let output = '';
+    repos.forEach(function (repo) {
+      output += `
+      <div class="card card-body mb-2">
+        <div class="row">
+          <div class="col-md-6">
+            <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+          </div>
+          <div class="col-md-6">
+            <span class="badge badge-primary">Stars: ${repo.stargazers_count}</span>
+            <span class="badge badge-primary">Watchers: ${repo.watchers_count}</span>
+            <span class="badge badge-primary">Forks: ${repo.forks_count}</span>
+          </div>
+        </div>
+      </div>
+      `
+    });
+    //Output repo
+    document.getElementById('repos').innerHTML = output;
   }
 
   //Show alert
