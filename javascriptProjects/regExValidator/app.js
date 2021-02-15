094a811d71f6dd8721c2dc0d47c3f27418c6b315
itssumitshukla <itssumitshukla@gmail.com>
2021-02-15 12:45:13 -1000

    setup zip validation and test

--- javascriptProjects/regExValidator/app.js
+++ javascriptProjects/regExValidator/app.js
@@ -1,5 +1,8 @@
 // Form Blur Event Listeners
 document.getElementById('name').addEventListener('blur', validateName);
+document.getElementById('zip').addEventListener('blur', validateZip);
+document.getElementById('email').addEventListener('blur', validateEmail);
+document.getElementById('phone').addEventListener('blur', validatePhone);
 
 //setup Function
 function validateName() {
@@ -10,5 +13,16 @@ function validateName() {
     name.classList.add('is-invalid');
   } else {
     name.classList.remove('is-invalid');
+  }
+}
+
+function validateZip() {
+  const zip = document.getElementById('zip');
+  const re = /^[0-9]{5}(-[0-9]{4})?$/;
+
+  if(!re.test(zip.value)){
+    zip.classList.add('is-invalid');
+  } else {
+    zip.classList.remove('is-invalid');
   }
 }
