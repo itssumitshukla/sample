c8f976f429dd7820f75e2d0b322e9229391af0fe
itssumitshukla <itssumitshukla@gmail.com>
2021-02-15 12:40:42 -1000

    setup blur event and test name validator using regex

---
+++ javascriptProjects/regExValidator/app.js
@@ -0,0 +1,14 @@
+// Form Blur Event Listeners
+document.getElementById('name').addEventListener('blur', validateName);
+
+//setup Function
+function validateName() {
+  const name = document.getElementById('name');
+  const re = /^[a-zA-Z]{2,10}$/;
+
+  if(!re.test(name.value)){
+    name.classList.add('is-invalid');
+  } else {
+    name.classList.remove('is-invalid');
+  }
+}
