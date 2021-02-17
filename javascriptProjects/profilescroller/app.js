cea98948cfbcb259c94832b122eb4cc53f18dae5
itssumitshukla <itssumitshukla@gmail.com>
2021-02-16 20:10:11 -1000

    test js and liveserver

--- javascriptProjects/profilescroller/app.js
+++ javascriptProjects/profilescroller/app.js
@@ -1,3 +1,4 @@
+//Fake data
 const data = [
   {
     name: 'John Doe',
@@ -25,44 +26,4 @@ const data = [
   }
 ];
 
-const profiles = profileIterator(data);
-
-// Call first profile
-nextProfile();
-
-// Next Event
-document.getElementById('next').addEventListener('click', nextProfile);
-
-// Next Profile Display
-function nextProfile() {
-  const currentProfile = profiles.next().value;
-
-  if(currentProfile !== undefined) {
-    document.getElementById('profileDisplay').innerHTML = `
-      <ul class="list-group">
-        <li class="list-group-item">Name: ${currentProfile.name}</li>
-        <li class="list-group-item">Age: ${currentProfile.age}</li>
-        <li class="list-group-item">Location: ${currentProfile.location}</li>
-        <li class="list-group-item">Preference: ${currentProfile.gender} looking for ${currentProfile.lookingfor}</li>
-      </ul>
-    `;
-
-    document.getElementById('imageDisplay').innerHTML = `<img src="${currentProfile.image}">`;
-  } else {
-    // No more profiles
-    window.location.reload();
-  }
-}
-
-// Profile Iterator
-function profileIterator(profiles) {
-  let nextIndex = 0;
-
-  return {
-    next: function() {
-      return nextIndex < profiles.length ? 
-      { value: profiles[nextIndex++], done: false } : 
-      { done: true }
-    }
-  };
-}
+console.log(data)
