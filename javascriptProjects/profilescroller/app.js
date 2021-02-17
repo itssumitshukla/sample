287c0be5060884ede27d31bc3055452c6d4df207
itssumitshukla <itssumitshukla@gmail.com>
2021-02-16 22:24:01 -1000

    fix generator function

--- javascriptProjects/profilescroller/app.js
+++ javascriptProjects/profilescroller/app.js
@@ -1,6 +1,5 @@
 //Fake data
-const data = [
-  {
+const data = [{
     name: 'John Doe',
     age: 32,
     gender: 'male',
@@ -25,5 +24,44 @@ const data = [
     image: 'https://randomuser.me/api/portraits/men/83.jpg'
   }
 ];
+const profiles = profileIterator(data);
 
-console.log(data)
+// Call first profile
+nextProfile();
+// Next Event
+document.getElementById('next').addEventListener('click', nextProfile);
+
+
+// Next Profile Display
+function nextProfile() {
+  const currentProfile = profiles.next().value;
+
+  if(currentProfile !== undefined) {
+    document.getElementById('profileDisplay').innerHTML = `
+      <ul class="list-group">
+        <li class="list-group-item">Name: ${currentProfile.name}</li>
+        <li class="list-group-item">Age: ${currentProfile.age}</li>
+        <li class="list-group-item">Location: ${currentProfile.location}</li>
+        <li class="list-group-item">Preference: ${currentProfile.gender} looking for ${currentProfile.lookingfor}</li>
+      </ul>
+    `;
+
+    document.getElementById('imageDisplay').innerHTML = `<img src="${currentProfile.image}">`;
+  } else {
+    // No more profiles
+    window.location.reload();
+  }
+}
+
+// Profile Iterator
+function profileIterator(profiles) {
+  let nextIndex = 0;
+
+  return {
+    next: function() {
+      return nextIndex < profiles.length ? 
+      { value: profiles[nextIndex++], done: false } : 
+      { done: true }
+    }
+  };
+}
