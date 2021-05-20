116afead6d48372580b472e6052fbade43cf7988
itssumitshukla <itssumitshukla@gmail.com>
2021-05-19 22:11:45 -1000

    setup dark mode function

--- javascriptProjects/lightDarkMode/script.js
+++ javascriptProjects/lightDarkMode/script.js
@@ -1,11 +1,41 @@
+//Root
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const nav = document.querySelector('#nav');
+const toggleIcon = document.getElementById('toggle-icon');
+const image1 = document.getElementById('image1');
+const image2 = document.getElementById('image2');
+const image3 = document.getElementById('image3');
+const textBox = document.querySelector('#text-box');
 
+//Dark Mode Function
+function darkMode(){
+  nav.getElementsByClassName.backgroundColor = 'rgb(0 0 0 / 50%)';
+  textBox.getElementsByClassName.backgroundColor = 'rgb(255 255 255 / 50%)';
+  toggleIcon.children[0].textContent = 'Dark Mode';
+  toggleIcon.children[1].classList.remove('fa-sun');
+  toggleIcon.children[1].classList.add('fa-moon');
+  image1.src = 'img/undraw_proud_coder_dark.svg';
+  image2.src = 'img/undraw_feeling_proud_dark.svg';
+  image3.src = 'img/undraw_conceptual_idea_dark.svg';
+}
+
+//Light Mode Function
+function lightMode(){
+  // nav.getElementsByClassName.backgroundColor = 'rgb(0 0 0 / 50%)';
+  // textBox.getElementsByClassName.backgroundColor = 'rgb(255 255 255 / 50%)';
+  // toggleIcon.children[0].textContent = 'Dark Mode';
+  // toggleIcon.children[1].classList.remove('fa-sun');
+  // toggleIcon.children[1].classList.add('fa-moon');
+  // image1.src = 'img/undraw_proud_coder_dark.svg';
+}
 //Switch Theme Dynamically
 function switchTheme(e){
   if(e.target.checked) {
     document.documentElement.setAttribute('data-theme', 'dark');
+    darkMode();
   } else {
     document.documentElement.setAttribute('data-theme', 'light');
+    lightMode();
   }
 }
 
