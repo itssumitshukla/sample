4837666f5e8a759f9f9793c9df7768daa7d815b7
itssumitshukla <itssumitshukla@gmail.com>
2021-05-19 22:18:20 -1000

    refactor image mode

--- javascriptProjects/lightDarkMode/script.js
+++ javascriptProjects/lightDarkMode/script.js
@@ -7,16 +7,20 @@ const image2 = document.getElementById('image2');
 const image3 = document.getElementById('image3');
 const textBox = document.querySelector('#text-box');
 
+//Dark or Light
+function imageMode(color){
+  image1.src = `img/undraw_proud_coder_${color}.svg`;
+  image2.src = `img/undraw_feeling_proud_${color}.svg`;
+  image3.src = `img/undraw_conceptual_idea_${color}.svg`;
+}
+
 //Dark Mode Function
 function darkMode(){
   nav.getElementsByClassName.backgroundColor = 'rgb(0 0 0 / 50%)';
   textBox.getElementsByClassName.backgroundColor = 'rgb(255 255 255 / 50%)';
   toggleIcon.children[0].textContent = 'Dark Mode';
-  toggleIcon.children[1].classList.remove('fa-sun');
-  toggleIcon.children[1].classList.add('fa-moon');
-  image1.src = 'img/undraw_proud_coder_dark.svg';
-  image2.src = 'img/undraw_feeling_proud_dark.svg';
-  image3.src = 'img/undraw_conceptual_idea_dark.svg';
+  toggleIcon.children[1].classList.replace('fa-sun','fa-moon');
+  imageMode('dark');
 }
 
 //Light Mode Function
@@ -24,11 +28,8 @@ function lightMode(){
   nav.getElementsByClassName.backgroundColor = 'rgb(255 255 255 / 50%)';
   textBox.getElementsByClassName.backgroundColor = 'rgb(0 0 0 / 50%)';
   toggleIcon.children[0].textContent = 'Light Mode';
-  toggleIcon.children[1].classList.remove('fa-moon');
-  toggleIcon.children[1].classList.add('fa-sun');
-  image1.src = 'img/undraw_proud_coder_light.svg';
-  image2.src = 'img/undraw_feeling_proud_light.svg';
-  image3.src = 'img/undraw_conceptual_idea_light.svg';
+  toggleIcon.children[1].classList.replace('fa-moon','fa-sun');
+  imageMode('light');
 }
 
 //Switch Theme Dynamically
