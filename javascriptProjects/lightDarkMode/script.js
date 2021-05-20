8d932d23975071909d2d533e34b5fdfd4b4531ba
itssumitshukla <itssumitshukla@gmail.com>
2021-05-19 22:14:37 -1000

    setup light function

--- javascriptProjects/lightDarkMode/script.js
+++ javascriptProjects/lightDarkMode/script.js
@@ -21,13 +21,16 @@ function darkMode(){
 
 //Light Mode Function
 function lightMode(){
-  // nav.getElementsByClassName.backgroundColor = 'rgb(0 0 0 / 50%)';
-  // textBox.getElementsByClassName.backgroundColor = 'rgb(255 255 255 / 50%)';
-  // toggleIcon.children[0].textContent = 'Dark Mode';
-  // toggleIcon.children[1].classList.remove('fa-sun');
-  // toggleIcon.children[1].classList.add('fa-moon');
-  // image1.src = 'img/undraw_proud_coder_dark.svg';
+  nav.getElementsByClassName.backgroundColor = 'rgb(255 255 255 / 50%)';
+  textBox.getElementsByClassName.backgroundColor = 'rgb(0 0 0 / 50%)';
+  toggleIcon.children[0].textContent = 'Light Mode';
+  toggleIcon.children[1].classList.remove('fa-moon');
+  toggleIcon.children[1].classList.add('fa-sun');
+  image1.src = 'img/undraw_proud_coder_light.svg';
+  image2.src = 'img/undraw_feeling_proud_light.svg';
+  image3.src = 'img/undraw_conceptual_idea_light.svg';
 }
+
 //Switch Theme Dynamically
 function switchTheme(e){
   if(e.target.checked) {
