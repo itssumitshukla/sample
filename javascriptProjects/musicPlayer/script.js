72871e5d71351b3531645b8085c56d904a47f427
itssumitshukla <itssumitshukla@gmail.com>
2021-05-26 22:35:35 -1000

    test play and pause function

--- javascriptProjects/musicPlayer/script.js
+++ javascriptProjects/musicPlayer/script.js
@@ -33,3 +33,25 @@ const songs = [
     artist: 'Metric/Jacinto Design',
   },
 ];
+
+// Check if Playing
+let isPlaying = false;
+
+// Play
+function playSong() {
+  isPlaying = true;
+  playBtn.classList.replace('fa-play', 'fa-pause');
+  playBtn.setAttribute('title', 'Pause');
+  music.play();
+}
+
+// Pause
+function pauseSong() {
+  isPlaying = false;
+  playBtn.classList.replace('fa-pause', 'fa-play');
+  playBtn.setAttribute('title', 'Play');
+  music.pause();
+}
+
+// Play or Pause Event Listener
+playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
