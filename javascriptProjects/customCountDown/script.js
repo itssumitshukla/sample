395b2eacfca54c8c75edc0f71b40642770c2e3c4
itssumitshukla <itssumitshukla@gmail.com>
2021-08-26 17:50:37 -1000

    setup input and event listners

--- javascriptProjects/customCountDown/script.js
+++ javascriptProjects/customCountDown/script.js
@@ -2,6 +2,20 @@ const inputContainer = document.getElementById('input-container');
 const countdownForm = document.getElementById('countdownForm');
 const dateEl = document.getElementById('date-picker');
 
+let countdownTitle = '';
+let countdownDate = '';
+
 //Set date input minimum
 const today = new Date().toISOString().split('T')[0];
 dateEl.setAttribute('min', today);
+
+//Take values from Form Input
+function updateCountdown(e) {
+  e.preventDefault();
+  countdownTitle = e.srcElement[0].value;
+  countdownDate = e.srcElement[1].value;
+  console.log(countdownDate, countdownTitle);
+}
+
+//Event Listner
+countdownForm.addEventListener('submit', updateCountdown);
