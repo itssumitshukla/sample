1a09b1b6b0ed000e31e90514042dfd9cd838eff0
itssumitshukla <itssumitshukla@gmail.com>
2021-08-27 20:21:05 -1000

    setup script to get values in/out of dom

--- javascriptProjects/customCountDown/script.js
+++ javascriptProjects/customCountDown/script.js
@@ -1,13 +1,25 @@
 const inputContainer = document.getElementById('input-container');
 const countdownForm = document.getElementById('countdownForm');
 const dateEl = document.getElementById('date-picker');
+const countdownEl = document.getElementById('countdown');
+const countdownElTitle = document.getElementById('countdown-title');
+const countdownBtn = document.getElementById('countdown-button');
+const timeElements = document.querySelectorAll('span');
 
 let countdownTitle = '';
 let countdownDate = '';
+let countdownValue = Date;
 
 //Set date input minimum
 const today = new Date().toISOString().split('T')[0];
 dateEl.setAttribute('min', today);
+
+//Populate Countdown/Complete UI
+function updateDOM() {
+  const now = new Date().getTime();
+  const distance = countdownValue - now;
+}
+
 
 //Take values from Form Input
 function updateCountdown(e) {
@@ -15,6 +27,10 @@ function updateCountdown(e) {
   countdownTitle = e.srcElement[0].value;
   countdownDate = e.srcElement[1].value;
   console.log(countdownDate, countdownTitle);
+  //Get number version of date
+  countdownValue = new Date(countdownDate).getTime();
+  console.log('Countdown Value: ', countdownValue);
+  updateDOM();
 }
 
 //Event Listner
