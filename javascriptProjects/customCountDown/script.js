96783f6ee3a407c1c3e0f64cf2b79b29d69396ad
itssumitshukla <itssumitshukla@gmail.com>
2021-08-27 20:53:34 -1000

    testing ui functionality

--- javascriptProjects/customCountDown/script.js
+++ javascriptProjects/customCountDown/script.js
@@ -10,6 +10,11 @@ let countdownTitle = '';
 let countdownDate = '';
 let countdownValue = Date;
 
+const second = 1000;
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24;
+
 //Set date input minimum
 const today = new Date().toISOString().split('T')[0];
 dateEl.setAttribute('min', today);
@@ -18,6 +23,26 @@ dateEl.setAttribute('min', today);
 function updateDOM() {
   const now = new Date().getTime();
   const distance = countdownValue - now;
+  console.log('Disdis: ', distance);
+
+  const days = Math.floor(distance/day);
+  const hours = Math.floor((distance % day) / hour);
+  const minutes = Math.floor((distance % hour) / minute);
+  const seconds = Math.floor((distance % minute) / second);
+  console.log(days, hours, minutes, seconds);
+
+  //Populating coundown
+  countdownElTitle.textContent = `${countdownTitle}`;
+  timeElements[0].textContent = `${days}`;
+  timeElements[1].textContent = `${hours}`;
+  timeElements[2].textContent = `${minutes}`;
+  timeElements[3].textContent = `${seconds}`;
+
+  //Hide input
+  inputContainer.hidden = true;
+
+  //SHow countdown
+  countdownEl.hidden = false;
 }
 
 
