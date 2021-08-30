be7ed8f97f52952a9c2d58759976e9234e5f54d3
itssumitshukla <itssumitshukla@gmail.com>
2021-08-29 16:18:57 -1000

    fix date issue for countdown value

--- javascriptProjects/customCountDown/script.js
+++ javascriptProjects/customCountDown/script.js
@@ -12,7 +12,7 @@ const completeBtn = document.getElementById('complete-button');
 
 let countdownTitle = '';
 let countdownDate = '';
-let countdownValue = Date;
+let countdownValue = new Date();
 let countdownActive;
 let savedCountdown;
 
