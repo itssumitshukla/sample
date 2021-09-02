4f8957d30e0a6427ec3dc77fb401ca30396c061b
itssumitshukla <itssumitshukla@gmail.com>
2021-09-01 17:01:43 -1000

    setup form event listner

--- javascriptProjects/bookKeeper/script.js
+++ javascriptProjects/bookKeeper/script.js
@@ -4,7 +4,8 @@ const modal = document.getElementById('modal');
 const modalShow = document.getElementById('show-modal');
 const modalClose = document.getElementById('close-modal');
 const bookmarkForm = document.getElementById('bookmark-form');
-const websiteNameEl = document.getElementById('website-url');
+const websiteNameEl = document.getElementById('website-name');
+const websiteUrlEl = document.getElementById('website-url');
 const bookmarksContainer = document.getElementById('bookmarks-container');
 
 //Show modal, focus on Input
@@ -21,4 +22,18 @@ modalClose.addEventListener('click', () => {
 
 window.addEventListener('click', (e) => {
   (e.target === modal ? modal.classList.remove('show-modal') : false);
-})
+});
+
+//Handle Data from Form
+function storeBookmark(e) {
+  e.preventDefault();
+  const nameValue = websiteNameEl.value;
+  let urlValue = websiteUrlEl.value;
+  if (!urlValue.includes('https://') && !urlValue.includes('http://')) {
+    urlValue = `https://${urlValue}`;
+  }
+  console.log(nameValue, urlValue)
+}
+
+//Event Listner
+bookmarkForm.addEventListener('submit', storeBookmark);
