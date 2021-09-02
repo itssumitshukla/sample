cde24610ba138490e581ffb9a33ddcf1ed94f4b8
itssumitshukla <itssumitshukla@gmail.com>
2021-09-01 16:37:31 -1000

    setup global variables and event lister

---
+++ javascriptProjects/bookKeeper/script.js
@@ -0,0 +1,17 @@
+//Setting up globabls
+
+const modal = document.getElementById('modal');
+const modalShow = document.getElementById('show-modal');
+const modalClose = document.getElementById('close-modal');
+const bookmarkForm = document.getElementById('bookmark-form');
+const websiteNameEl = document.getElementById('website-url');
+const bookmarksContainer = document.getElementById('bookmarks-container');
+
+//Show modal, focus on Input
+function showModal() {
+  modal.classList.add('show-modal');
+  websiteNameEl.focus();
+}
+
+//Modal Event listner
+modalShow.addEventListener('click', showModal);
