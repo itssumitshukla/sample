908dce2248c5a1c015a50f3bd6dd4bb52ee7d962
itssumitshukla <itssumitshukla@gmail.com>
2021-09-04 16:22:58 -1000

    setup filter function

--- javascriptProjects/RenderingMovies/assets/scripts/script.js
+++ javascriptProjects/RenderingMovies/assets/scripts/script.js
@@ -3,7 +3,7 @@ const searchBtn = document.getElementById('search-btn');
 
 const movies = [];
 
-const renderMovies = () => {
+const renderMovies = (filter = '') => {
   const movieList = document.getElementById('movie-list');
   if (movies.length === 0) {
     movieList.classList.remove('visible');
@@ -13,11 +13,13 @@ const renderMovies = () => {
   }
   movieList.innerHTML = '';
 
-  movies.forEach((movie) => {
+  const filteredMovies = !filter ? movies : movies.filter(movie =>movie.info.title.includes(filter));
+
+    filteredMovies.forEach((movie) => {
     const movieEl = document.createElement('li');
     let text = movie.info.title + ' - ';
-    for ( const key in movie.info){
-      if (key !== "title"){
+    for (const key in movie.info) {
+      if (key !== "title") {
         text = text + `${key}: ${movie.info[key]}`;
       }
     }
@@ -52,4 +54,10 @@ const addMovieHandler = () => {
   renderMovies();
 };
 
+const searchMovieHandler = () => {
+  const filterTerm = document.getElementById('filter-title').value;
+  renderMovies(filterTerm);
+}
+
 addMovieBtn.addEventListener('click', addMovieHandler);
+searchBtn.addEventListener('click', searchMovieHandler);
