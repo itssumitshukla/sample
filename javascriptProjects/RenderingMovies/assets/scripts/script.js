2eee3ede34e262c210cfb83e0ad9a266d9e00b5b
itssumitshukla <itssumitshukla@gmail.com>
2021-09-05 20:51:05 -1000

    setup obj destructure in filter movie function

--- javascriptProjects/RenderingMovies/assets/scripts/script.js
+++ javascriptProjects/RenderingMovies/assets/scripts/script.js
@@ -17,10 +17,13 @@ const renderMovies = (filter = '') => {
 
     filteredMovies.forEach((movie) => {
     const movieEl = document.createElement('li');
-    let text = movie.info.title + ' - ';
-    for (const key in movie.info) {
+    const { info, ...otherProps } = movie;
+    console.log(otherProps);
+    const { title: movieTitle } = info;
+    let text = movieTitle + ' - ';
+    for (const key in info) {
       if (key !== "title") {
-        text = text + `${key}: ${movie.info[key]}`;
+        text = text + `${key}: ${info[key]}`;
       }
     }
     movieEl.textContent = text;
