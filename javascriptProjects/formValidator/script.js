f7dfb009765f56a7cfe508781fd2fa700b77c58c
itssumitshukla <itssumitshukla@gmail.com>
2021-09-29 14:33:24 -1000

    added novalidate to tst form

--- javascriptProjects/formValidator/script.js
+++ javascriptProjects/formValidator/script.js
@@ -3,11 +3,22 @@ const password1El = document.getElementById('password1');
 const password2El = document.getElementById('password2');
 const messageContainer = document.querySelector('.message-container');
 const message = document.getElementById('message');
+let isValid = false;
 
+function validateForm() {
+  //Title
+  isValid = form.checkValidity();
+  //Style
+  message.textContent = 'Please fill out all Fields!';
+  message.style.color = 'red';
+  messageContainer.style.borderColor = 'red';
+}
 
 function processFormData(e) {
   e.preventDefault();
-  console.log(e);
+
+  //Method
+  validateForm();
 }
 
 //Event Listners
