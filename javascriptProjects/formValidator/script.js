b168ea71be22e64fe33c16a73019a59facc6e0f2
itssumitshukla <itssumitshukla@gmail.com>
2021-09-29 14:38:00 -1000

    setup password 1 and 2 match condition

--- javascriptProjects/formValidator/script.js
+++ javascriptProjects/formValidator/script.js
@@ -4,14 +4,30 @@ const password2El = document.getElementById('password2');
 const messageContainer = document.querySelector('.message-container');
 const message = document.getElementById('message');
 let isValid = false;
+let passwordsMatch = false;
 
 function validateForm() {
   //Title
   isValid = form.checkValidity();
   //Style
-  message.textContent = 'Please fill out all Fields!';
-  message.style.color = 'red';
-  messageContainer.style.borderColor = 'red';
+  if (!isValid) {
+    message.textContent = 'Please fill out all Fields!';
+    message.style.color = 'red';
+    messageContainer.style.borderColor = 'red';
+  }
+  //Check if passwords match
+  if (password1El.value === password2El.value) {
+    passwordsMatch = true;
+    password1El.style.borderColor = 'green';
+    password2El.style.borderColor = 'green';
+  } else {
+    passwordsMatch = false;
+    message.textContent = 'Make sure passwords match!';
+    message.style.color = 'red';
+    messageContainer.style.borderColor = 'red';
+    password1El.style.borderColor = 'red';
+    password2El.style.borderColor = 'red';
+  }
 }
 
 function processFormData(e) {
