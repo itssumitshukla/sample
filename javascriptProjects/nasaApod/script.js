942ba4c9dfda71ce80ed7356330621510be6849c
itssumitshukla <itssumitshukla@gmail.com>
2021-10-16 21:21:46 -1000

    setup js

---
+++ javascriptProjects/nasaApod/script.js
@@ -0,0 +1,5 @@
+const resultsNav = document.getElementById('resultsNav');
+const favoritesNav = document.getElementById('favoritesNav');
+const imagesContainer = document.querySelector('.images-container');
+const saveConfirmed = document.querySelector('.save-confirmed');
+const loader = document.querySelector('.loader');
