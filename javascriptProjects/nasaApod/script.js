4a11d58160d42928cab5a8490d1a9a6dd5ef74e0
itssumitshukla <itssumitshukla@gmail.com>
2021-10-18 19:19:01 -1000

    setup nasa demo api

--- javascriptProjects/nasaApod/script.js
+++ javascriptProjects/nasaApod/script.js
@@ -3,3 +3,19 @@ const favoritesNav = document.getElementById('favoritesNav');
 const imagesContainer = document.querySelector('.images-container');
 const saveConfirmed = document.querySelector('.save-confirmed');
 const loader = document.querySelector('.loader');
+
+//Nasa API
+const count = 10;
+const apiKey = 'DEMO_KEY';
+const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count={count}`;
+
+let resultsArray = [];
+
+//Get 10 images from Nasa Api
+async function getNasaPictures(){
+  try{
+
+  } catch(error){}
+  //
+  console.log(console.error();)
+}
