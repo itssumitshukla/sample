8bf9b7a1b991f362fef3ae6a5a05c755eac08044
itssumitshukla <itssumitshukla@gmail.com>
2021-10-18 19:22:32 -1000

    test fetch/await nasa api

--- javascriptProjects/nasaApod/script.js
+++ javascriptProjects/nasaApod/script.js
@@ -7,15 +7,22 @@ const loader = document.querySelector('.loader');
 //Nasa API
 const count = 10;
 const apiKey = 'DEMO_KEY';
-const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count={count}`;
+const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${count}`;
 
 let resultsArray = [];
 
 //Get 10 images from Nasa Api
-async function getNasaPictures(){
-  try{
+async function getNasaPictures() {
+  try {
+    const response = await fetch(apiUrl);
+    resultsArray = await response.json();
+    console.log(resultsArray)
+  } catch (error) {
+    //
+    console.log(error)
+  }
 
-  } catch(error){}
-  //
-  console.log(console.error();)
 }
+
+//on Load
+getNasaPictures();
