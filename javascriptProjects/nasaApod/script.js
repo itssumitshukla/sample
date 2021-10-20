8edd752d1835a38a55eb9ba677d1ebc875ca4f93
itssumitshukla <itssumitshukla@gmail.com>
2021-10-19 17:57:38 -1000

    setup all card/footer element

--- javascriptProjects/nasaApod/script.js
+++ javascriptProjects/nasaApod/script.js
@@ -5,9 +5,9 @@ const saveConfirmed = document.querySelector('.save-confirmed');
 const loader = document.querySelector('.loader');
 
 //Nasa API
-const count = 10;
-const apiKey = 'DEMO_KEY';
-const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${count}`;
+// const count = 10;
+// const apiKey = 'DEMO_KEY';
+// const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${count}`;
 
 let resultsArray = [];
 
@@ -28,7 +28,31 @@ function updateDOM() {
     image.alt = 'Nasa Picture of the day';
     image.loading = 'lazy';
     image.classList.add('card-img-top');
-  })
+    //Card Body
+    const cardBody = document.createElement('div');
+    cardBody.classList.add('card-body');
+    //Card Title
+    const cardTitle = document.createElement('h5');
+    cardTitle.classList.add('card-title');
+    cardTitle.textContent = result.title;
+    //Save Text
+    const saveText = document.createElement('p');
+    saveText.classList.add('clickable');
+    saveText.textContent = 'Add to Favorites';
+    //Card Text
+    const cardText = document.createElement('p');
+    cardText.textContent = result.explanation;
+    //Footer Container
+    const footer = document.createElement('small');
+    footer.classList.add('text-muted');
+    //Date
+    const date = document.createElement('strong');
+    date.textContent = result.date;
+        // Copyright
+        const copyrightResult = result.copyright === undefined ? '' : result.copyright;
+        const copyright = document.createElement('span');
+        copyright.textContent = ` ${copyrightResult}`;
+  });
 }
 
 //Get 10 images from Nasa Api
