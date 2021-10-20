644a8a03d9ff9cadb536ae256474964cadab38d6
itssumitshukla <itssumitshukla@gmail.com>
2021-10-19 17:52:22 -1000

    setup dynamic update dom function

--- javascriptProjects/nasaApod/script.js
+++ javascriptProjects/nasaApod/script.js
@@ -11,12 +11,33 @@ const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${co
 
 let resultsArray = [];
 
+//Update DOM
+function updateDOM() {
+  resultsArray.forEach((result) => {
+    //Card COntainer
+    const card = document.createElement('div');
+    card.classList.add('card');
+    //Link
+    const link = document.createElement('a');
+    link.href = result.hdurl;
+    link.title = 'View Full Image';
+    link.target = '_blank';
+    //Image
+    const image = document.createElement('img');
+    image.src = result.url;
+    image.alt = 'Nasa Picture of the day';
+    image.loading = 'lazy';
+    image.classList.add('card-img-top');
+  })
+}
+
 //Get 10 images from Nasa Api
 async function getNasaPictures() {
   try {
     const response = await fetch(apiUrl);
     resultsArray = await response.json();
-    console.log(resultsArray)
+    console.log(resultsArray);
+    updateDOM();
   } catch (error) {
     //
     console.log(error)
