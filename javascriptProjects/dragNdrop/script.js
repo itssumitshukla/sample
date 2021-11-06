cfd1b6854af54448084eb57979aacb1a89319f90
itssumitshukla <itssumitshukla@gmail.com>
2021-11-05 20:48:42 -1000

    setup js

---
+++ javascriptProjects/dragNdrop/script.js
@@ -0,0 +1,12 @@
+const addBtns = document.querySelectorAll('.add-btn:not(.solid)');
+const saveItemBtns = document.querySelectorAll('.solid');
+const addItemContainers = document.querySelectorAll('.add-container');
+const addItems = document.querySelectorAll('.add-item');
+// Item Lists
+const listColumns = document.querySelectorAll('.drag-item-list');
+const backlogListEl = document.getElementById('backlog-list');
+const progressListEl = document.getElementById('progress-list');
+const completeListEl = document.getElementById('complete-list');
+const onHoldListEl = document.getElementById('on-hold-list');
+
+console.log(addBtns);
