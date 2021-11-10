f33013bdf9f5a8b3010b2c825f6a1c1adf1711ad
itssumitshukla <itssumitshukla@gmail.com>
2021-11-09 17:36:55 -1000

    setup update item function and update html with all ootb ondrag function

--- javascriptProjects/dragNdrop/script.js
+++ javascriptProjects/dragNdrop/script.js
@@ -104,6 +104,20 @@ function updateDOM() {
   updateSavedColumns();
 }
 
+// Update Item - Delete if necessary, or update Array value
+function updateItem(id, column) {
+  const selectedArray = listArrays[column];
+  const selectedColumn = listColumns[column].children;
+  if (!dragging) {
+    if (!selectedColumn[id].textContent) {
+      delete selectedArray[id];
+    } else {
+      selectedArray[id] = selectedColumn[id].textContent;
+    }
+    updateDOM();
+  }
+}
+
 // When Item Starts Dragging
 function drag(e) {
   draggedItem = e.target;
