3bcd9d4148ed83fe3fb58c7fdb0407ac582308cf
itssumitshukla <itssumitshukla@gmail.com>
2021-12-06 22:47:48 -1000

    test js

---
+++ javascriptProjects/jqueryProject/test/script.js
@@ -0,0 +1,39 @@
+let quotes = [
+  {
+    quote:
+      "Life is too short and sweet to be spent by cribbing and complaining about things. Here are some random quotes about the most wonderful gift that we've got",
+    author: " Life"
+  },
+  {
+    quote:
+      "Humor is richly rewarding to the person who employs it. It has some value in gaining and holding attention. But it has no persuasive value at all",
+    author: "John Kenneth Galbraith"
+  },
+  {
+    quote:
+      "God save me from my friends. I can protect myself from my enemies.",
+    author: "Claude Louis Hector de Villars "
+  },
+  {
+    quote: "The price of anything is the amount of life you exchange for it.",
+    author: "David Thoreau"
+  },
+  {
+    quote:
+      "Life is like a landscape. You live in the midst of it but can describe it only from the vantage point of distance. ",
+    author: "Charles Lindbergh"
+  },
+  {
+    quote:
+      "A critic is someone who never actually goes to the battle, yet who afterwards comes out shooting the wounded.",
+    author: " Tyne Daly"
+  }
+];
+
+let $btn = $('#btn');
+$btn.click(function () {
+  let number = Math.floor(Math.random()*quotes.length);
+  $('#quote').html('<span>"</span>' + quotes[number].quote + '<span>"</span>');
+  $('#auther').html('<span>--</span>' + quotes[number].author);
+  console.log('YAY')
+});
