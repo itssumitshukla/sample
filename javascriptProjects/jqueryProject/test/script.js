5004da88a88cd29c7f7d9128cf32aec09538fb5b
itssumitshukla <itssumitshukla@gmail.com>
2021-12-15 22:35:14 -1000

    test agency layout

--- javascriptProjects/jqueryProject/test/script.js
+++ javascriptProjects/jqueryProject/test/script.js
@@ -33,23 +33,23 @@
 // });
 
 //assign variables
-const $message = $('.message');
-const $text = $('#text');
-const $send = $('#send');
-const $clear = $('#clear');
+// const $message = $('.message');
+// const $text = $('#text');
+// const $send = $('#send');
+// const $clear = $('#clear');
 
-//click event
-$send.click(deliverMessage);
+// //click event
+// $send.click(deliverMessage);
 
-function deliverMessage (){
-  let textValue = $text.val();
-  $message.html(textValue);
-}
+// function deliverMessage() {
+//   let textValue = $text.val();
+//   $message.html(textValue);
+// }
 
-//Clear
-$clear.click(clearField);
+// //Clear
+// $clear.click(clearField);
 
-function clearField() {
-  $message.html('');
-  $text.val('');
-}
+// function clearField() {
+//   $message.html('');
+//   $text.val('');
+// }
