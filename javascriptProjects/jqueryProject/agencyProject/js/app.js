09a0b6fa4869033e13d51414c44246b4460585b1
itssumitshukla <itssumitshukla@gmail.com>
2022-04-11 20:52:22 -1000

    setup play pause in js

--- javascriptProjects/jqueryProject/agencyProject/js/app.js
+++ javascriptProjects/jqueryProject/agencyProject/js/app.js
@@ -5,4 +5,15 @@ $(document).ready(function(){
   $('.navBtn').click(function(){
     $('.nav').toggleClass('nav--show')
   });
+  //video
+  $('.video__switch-container').click(function(){
+    let value = $('.video__switch-btn').hasClass('btnSlide');
+    if(value){
+      $('.video__switch-btn').removeClass('btnSlide');
+      $('.video').get(0).play();
+    } else {
+     $('.video__switch-btn').addClass('btnSlide');
+     $('.video').get(0).pause();
+    }
+  })
 });
