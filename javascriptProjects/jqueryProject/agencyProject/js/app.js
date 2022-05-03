3bc775b76c133cea39abc2904ef44c6d29748deb
itssumitshukla <itssumitshukla@gmail.com>
2022-05-02 21:46:08 -1000

    testing accordion function

--- javascriptProjects/jqueryProject/agencyProject/js/app.js
+++ javascriptProjects/jqueryProject/agencyProject/js/app.js
@@ -1,30 +1,35 @@
-$(document).ready(function(){
+$(document).ready(function () {
   $('.preloader').fadeOut();
 
   //navbar
-  $('.navBtn').click(function(){
+  $('.navBtn').click(function () {
     $('.nav').toggleClass('nav--show')
   });
   //video
-  $('.video__switch-container').click(function(){
+  $('.video__switch-container').click(function () {
     let value = $('.video__switch-btn').hasClass('btnSlide');
-    if(value){
+    if (value) {
       $('.video__switch-btn').removeClass('btnSlide');
       $('.video').get(0).play();
     } else {
-     $('.video__switch-btn').addClass('btnSlide');
-     $('.video').get(0).pause();
+      $('.video__switch-btn').addClass('btnSlide');
+      $('.video').get(0).pause();
     }
   })
 
-  //Magnific popup
- $('#projects__modal').magnificPopup({
-  delegate: 'a', // child items selector, by clicking on it popup will open
-  type: 'image',
-  gallery: {
-   // options for gallery
-   enabled: true
-  }
-  // other options
- });
+  // magnific popup
+  // $('#projects__modal').magnificPopup({
+  //   delegate: 'a', // child items selector, by clicking on it popup will open
+  //   type: 'image',
+  //   gallery: {
+  //     // options for gallery
+  //     enabled: true
+  //   }
+  //   // other options
+  // });
+  //accordian
+  $('.accordion__btn').click(function () {
+    $(this).toggleClass('rotate')
+  })
+
 });
