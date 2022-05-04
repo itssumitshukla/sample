267cb94bc159b09750568a856480fba7b2280c54
itssumitshukla <itssumitshukla@gmail.com>
2022-05-03 21:34:04 -1000

    refactor html and fix number section

--- javascriptProjects/jqueryProject/agencyProject/js/app.js
+++ javascriptProjects/jqueryProject/agencyProject/js/app.js
@@ -18,15 +18,15 @@ $(document).ready(function () {
   })
 
   // magnific popup
-  // $('#projects__modal').magnificPopup({
-  //   delegate: 'a', // child items selector, by clicking on it popup will open
-  //   type: 'image',
-  //   gallery: {
-  //     // options for gallery
-  //     enabled: true
-  //   }
-  //   // other options
-  // });
+  $('#projects__modal').magnificPopup({
+    delegate: 'a', // child items selector, by clicking on it popup will open
+    type: 'image',
+    gallery: {
+      // options for gallery
+      enabled: true
+    }
+    // other options
+  });
   //accordian
   $('.accordion__btn').click(function () {
     $('.accordion__btn').not(this).removeClass('rotate');
