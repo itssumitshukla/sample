225c57ace5271e1e09cd9ba38172ceabc9e277f0
itssumitshukla <itssumitshukla@gmail.com>
2023-11-28 11:21:46 -1000

    setup initial if else statement

--- javascriptProjects/guessMyNumber/script.js
+++ javascriptProjects/guessMyNumber/script.js
@@ -7,3 +7,17 @@ let highscore = 0;
 const displayMessage = function (message) {
   document.querySelector(".message").textContent = message;
 };
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  // When there is no input
+  if (!guess) {
+    // document.querySelector('.message').textContent = '⛔️ No number!';
+    displayMessage('⛔️ No number!');
+
+    // When player wins
+  } else if (guess === secretNumber) {
+    console.log('Test');
+  }}
