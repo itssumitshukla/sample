837f84c6785c69155810b91fafec194e1096d9f5
itssumitshukla <itssumitshukla@gmail.com>
2023-11-28 11:20:09 -1000

    setting up initial js

---
+++ javascriptProjects/guessMyNumber/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector(".message").textContent = message;
+};
