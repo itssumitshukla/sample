d647b4d861e456615f697c177e5fe3c4c3cd9776
itssumitshukla <itssumitshukla@gmail.com>
2023-11-29 10:50:01 -1000

    setup js file

---
+++ javascriptProjects/modalWindow/script.js
@@ -0,0 +1,8 @@
+'use strict'
+
+let modal = document.querySelector('.modal');
+let overlay = document.querySelector('.overlay');
+let btn = document.querySelector('.close-modal');
+let btnsOpenModal = document.querySelector('.open-modal');
+
+console.log(btnsOpenModal);
