0a67644ccf5f7446aedd8c4ccecdf81ce32169e5
itssumitshukla <itssumitshukla@gmail.com>
2023-11-29 10:51:58 -1000

    setup modal loop

--- javascriptProjects/modalWindow/script.js
+++ javascriptProjects/modalWindow/script.js
@@ -3,6 +3,10 @@
 let modal = document.querySelector('.modal');
 let overlay = document.querySelector('.overlay');
 let btn = document.querySelector('.close-modal');
-let btnsOpenModal = document.querySelector('.open-modal');
+let btnsOpenModal = document.querySelectorAll('.show-modal');
 
 console.log(btnsOpenModal);
+
+for(let i = 0; i < btnsOpenModal.length; i++){
+    console.log(btnsOpenModal[i].textContent);
+}
