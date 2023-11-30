af3ef099274ad7cb23789b90e0618525ecfa0a80
itssumitshukla <itssumitshukla@gmail.com>
2023-11-29 19:47:20 -1000

    fix the modal param

--- javascriptProjects/modalWindow/script.js
+++ javascriptProjects/modalWindow/script.js
@@ -5,17 +5,25 @@ let overlay = document.querySelector('.overlay');
 let btnCloseModal = document.querySelector('.close-modal');
 let btnsOpenModal = document.querySelectorAll('.show-modal');
 
-console.log(btnsOpenModal);
+
+ let openModal = function () {
+ modal.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+    });
+
+ let closeModal = function () {
+        modal.classList.add('hidden');
+        overlay.classList.add('hidden');
+};
+
 
 for(let i = 0; i < btnsOpenModal.length; i++){
     btnsOpenModal[i].addEventListener('click', function(){
         console.log('BUTTON CLICKED');
-        modal.classList.remove('hidden');
-        overlay.classList.remove('hidden');
-    });
-};
+       
+})};
 
-btnCloseModal.addEventListener('click', function () {
-        modal.classList.add('hidden');
-        overlay.classList.add('hidden');
-});
+
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
