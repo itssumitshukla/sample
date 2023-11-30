8381ae30740163475bbb8e83cb9434d103ec9e35
itssumitshukla <itssumitshukla@gmail.com>
2023-11-29 19:32:12 -1000

    fix close btn modal

--- javascriptProjects/modalWindow/script.js
+++ javascriptProjects/modalWindow/script.js
@@ -2,7 +2,7 @@
 
 let modal = document.querySelector('.modal');
 let overlay = document.querySelector('.overlay');
-let btn = document.querySelector('.close-modal');
+let btnCloseModal = document.querySelector('.close-modal');
 let btnsOpenModal = document.querySelectorAll('.show-modal');
 
 console.log(btnsOpenModal);
@@ -11,5 +11,11 @@ for(let i = 0; i < btnsOpenModal.length; i++){
     btnsOpenModal[i].addEventListener('click', function(){
         console.log('BUTTON CLICKED');
         modal.classList.remove('hidden');
+        overlay.classList.remove('hidden');
     });
-}
+};
+
+btnCloseModal.addEventListener('click', function () {
+        modal.classList.add('hidden');
+        overlay.classList.add('hidden');
+});
