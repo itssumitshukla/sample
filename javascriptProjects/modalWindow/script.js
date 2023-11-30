41bf4a5b1ea021a320153b16f021905799f83652
itssumitshukla <itssumitshukla@gmail.com>
2023-11-29 19:48:24 -1000

    refactor code

--- javascriptProjects/modalWindow/script.js
+++ javascriptProjects/modalWindow/script.js
@@ -9,8 +9,7 @@ let btnsOpenModal = document.querySelectorAll('.show-modal');
  let openModal = function () {
  modal.classList.remove('hidden');
         overlay.classList.remove('hidden');
-    });
-
+    };
  let closeModal = function () {
         modal.classList.add('hidden');
         overlay.classList.add('hidden');
@@ -19,10 +18,8 @@ let btnsOpenModal = document.querySelectorAll('.show-modal');
 
 for(let i = 0; i < btnsOpenModal.length; i++){
     btnsOpenModal[i].addEventListener('click', function(){
-        console.log('BUTTON CLICKED');
-       
+        openModal();
 })};
-
 
 
 btnCloseModal.addEventListener('click', closeModal);
