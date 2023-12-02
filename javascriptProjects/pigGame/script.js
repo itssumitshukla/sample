08299e8065d43b34b22bee08349cadf407f2c028
itssumitshukla <itssumitshukla@gmail.com>
2023-12-01 15:08:28 -1000

    setup js for the pig game and added dice images to html

---
+++ javascriptProjects/pigGame/script.js
@@ -0,0 +1,16 @@
+'use strict'
+
+//initial
+let player0El = document.querySelector('.player--0');
+let player1El = document.querySelector('.player--1');
+let score0El = document.querySelector('#score--0');
+let score1El = document.getElementById('score--1');
+let current0El = document.getElementById('current--0');
+let current1El = document.getElementById('current--1');
+
+let diceEl = document.querySelector('.dice');
+let btnNew = document.querySelector('.btn--new');
+let btnRoll = document.querySelector('.btn--roll');
+let btnHold = document.querySelector('.btn--hold');
+
+let scores, currentScore, activePlayer, playing;
