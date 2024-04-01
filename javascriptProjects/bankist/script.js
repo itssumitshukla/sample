f7e9ce05b5c6ef18c131aee270efc4f22a7e4513
itssumitshukla <itssumitshukla@gmail.com>
2024-04-01 11:48:51 -1000

    setting up new map function for euro to usd conversion

--- javascriptProjects/bankist/script.js
+++ javascriptProjects/bankist/script.js
@@ -62,12 +62,13 @@ const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
 const displayMovements = function(movements){
+    containerMovements.innerHTML = '';
     movements.forEach(function(mov, i) {
         const type = mov > 0 ? 'deposit':'withdrawal';
     const html = `
           <div class="movements">
         <div class="movements__row">
-          <div class="movements__type movements__type--${type}">${i + 1}</div>
+          <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
           <div class="movements__value">${mov}</div>
         </div>
     `;
@@ -90,3 +91,9 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+const euroToUsd = 1.1;
+
+movements.map(function (mov) {
+  return mov * euroToUsd;
+})
