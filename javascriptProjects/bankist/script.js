61f1c150df1ccbc8cf1bab950cdcf0896d5903bd
itssumitshukla <itssumitshukla@gmail.com>
2024-04-02 11:26:20 -1000

    testing username split

--- javascriptProjects/bankist/script.js
+++ javascriptProjects/bankist/script.js
@@ -78,36 +78,40 @@ const displayMovements = function(movements){
 
 displayMovements(account1.movements)
 
+const user = 'Sumit test user';
+const username = user.toLowerCase().split(' ');
+console.log(username);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-/////////////////////////////////////////////////
+// /////////////////////////////////////////////////
 
-const euroToUsd = 1.1;
+// const euroToUsd = 1.1;
 
-const movementsUSD = movements.map(function (mov) {
-  return mov * euroToUsd;
-});
+// const movementsUSD = movements.map(function (mov) {
+//   return mov * euroToUsd;
+// });
 
-console.log(euroToUsd);
-console.log(movementsUSD);
+// console.log(euroToUsd);
+// console.log(movementsUSD);
 
-const movementUSDfor = [];
+// const movementUSDfor = [];
 
-for(const mov of movements){
-  movementUSDfor.push(mov * euroToUsd)
-}
+// for(const mov of movements){
+//   movementUSDfor.push(mov * euroToUsd)
+// }
 
-const movementDescriptions = movements.map((mov, i )=>
-  `Movement ${i+1}: You ${mov > 0? 'deposited': 'withdrew'} ${Math.abs(mov)}`);
+// const movementDescriptions = movements.map((mov, i )=>
+//   `Movement ${i+1}: You ${mov > 0? 'deposited': 'withdrew'} ${Math.abs(mov)}`);
 
-console.log(movementDescriptions);
+// console.log(movementDescriptions);
