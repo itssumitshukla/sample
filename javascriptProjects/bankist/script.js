c90e16500e91e11a50215d39ea0e1be1bd389977
itssumitshukla <itssumitshukla@gmail.com>
2024-04-08 14:03:58 -1000

    testing filter method

--- javascriptProjects/bankist/script.js
+++ javascriptProjects/bankist/script.js
@@ -126,3 +126,12 @@ console.log(accounts);
 //   `Movement ${i+1}: You ${mov > 0? 'deposited': 'withdrew'} ${Math.abs(mov)}`);
 
 // console.log(movementDescriptions);
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const deposits = movements.filter(function(mov){
+  return mov > 0;
+});
+
+console.log(movements);
+console.log(deposits);
