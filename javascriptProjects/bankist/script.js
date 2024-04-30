c3e764ce651d4452bfe402b48b772efc86f81bca
itssumitshukla <itssumitshukla@gmail.com>
2024-04-29 14:45:04 -1000

    testing find index logic for close accounbt

--- javascriptProjects/bankist/script.js
+++ javascriptProjects/bankist/script.js
@@ -186,6 +186,11 @@ btnClose.addEventListener("click", function (e) {
     inputCloseUsername.value === currentAccount.username &&
     Number(inputClosePin.value) === currentAccount.pin
   ) {
+    const index = accounts.findIndex(
+      (acc) => acc.username === currentAccount.username
+    );
+    console.log(index);
+    //accounts.splice(index,1);
   }
 });
 
