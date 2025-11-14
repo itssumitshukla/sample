c953c7a76c0a534d918f322b761fbaceddaeecf9
itssumitshukla <itssumitshukla@gmail.com>
2025-11-13 15:51:20 -1000

    setup new canvas outline

--- javascriptProjects/maze/index.js
+++ javascriptProjects/maze/index.js
@@ -1,29 +1,29 @@
 //from Matter.js
-const {
-  Engine,
-  Render,
-  Runner,
-  World,
-  Bodies
-} = Matter;
+const { Engine, Render, Runner, World, Bodies } = Matter;
 
 const engine = Engine.create();
-const {
-  world
-} = engine;
+const { world } = engine;
 const render = Render.create({
   element: document.body,
   engine: engine,
   options: {
     width: 800,
-    height: 600
-  }
+    height: 600,
+  },
 });
 
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
-const shape = Bodies.rectangle(200, 200, 50, 50, {
-  isStatic: true
-});
-World.add(world, shape);
+//Walls
+const walls = [
+  Bodies.rectangle(400, 0, 800, 40, { isStatic: true }),
+  Bodies.rectangle(400, 600, 800, 40, { isStatic: true }),
+];
+World.add(world, walls);
+
+// Old Code
+//const shape = Bodies.rectangle(200, 200, 50, 50, {
+//   isStatic: true,
+// });
+// World.add(world, shape);
