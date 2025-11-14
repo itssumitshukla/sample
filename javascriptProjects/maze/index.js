7d97556c1a317987e88f97e0ca7212baf0430e10
itssumitshukla <itssumitshukla@gmail.com>
2025-11-13 15:57:38 -1000

    fix walls and add drop functionality

--- javascriptProjects/maze/index.js
+++ javascriptProjects/maze/index.js
@@ -19,8 +19,12 @@ Runner.run(Runner.create(), engine);
 const walls = [
   Bodies.rectangle(400, 0, 800, 40, { isStatic: true }),
   Bodies.rectangle(400, 600, 800, 40, { isStatic: true }),
+  Bodies.rectangle(0, 300, 40, 600, { isStatic: true }),
+  Bodies.rectangle(800, 300, 40, 600, { isStatic: true }),
 ];
 World.add(world, walls);
+
+World.add(world, Bodies.rectangle(200, 200, 50, 50));
 
 // Old Code
 //const shape = Bodies.rectangle(200, 200, 50, 50, {
