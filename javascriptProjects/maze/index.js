993c7272df8ad3050931d3d47b070429733ce310
itssumitshukla <itssumitshukla@gmail.com>
2025-11-14 17:27:42 -1000

    setting up stacks on canvas

--- javascriptProjects/maze/index.js
+++ javascriptProjects/maze/index.js
@@ -2,14 +2,17 @@
 const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse } =
   Matter;
 
+const width = 800;
+const height = 600;
+
 const engine = Engine.create();
 const { world } = engine;
 const render = Render.create({
   element: document.body,
   engine: engine,
   options: {
-    width: 800,
-    height: 600,
+    width,
+    height,
   },
 });
 
@@ -32,7 +35,10 @@ const walls = [
 ];
 World.add(world, walls);
 
-World.add(world, Bodies.rectangle(200, 200, 50, 50));
+//Random Shapes
+for (let i = 0; i < 20; i++) {
+  World.add(world, Bodies.rectangle(200, 200, 50, 50));
+}
 
 // Old Code
 //const shape = Bodies.rectangle(200, 200, 50, 50, {
