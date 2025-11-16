a79906d7b89b7221ec3d5e3329f73f5a42b62c68
itssumitshukla <itssumitshukla@gmail.com>
2025-11-15 14:32:00 -1000

    setup maze pattern

--- javascriptProjects/maze/index.js
+++ javascriptProjects/maze/index.js
@@ -1,8 +1,7 @@
 //from Matter.js
-const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse } =
-  Matter;
+const { Engine, Render, Runner, World, Bodies } = Matter;
 
-const width = 800;
+const width = 600;
 const height = 600;
 
 const engine = Engine.create();
@@ -11,7 +10,7 @@ const render = Render.create({
   element: document.body,
   engine: engine,
   options: {
-    wireframes: false,
+    wireframes: true,
     width,
     height,
   },
@@ -20,43 +19,42 @@ const render = Render.create({
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
-World.add(
-  world,
-  MouseConstraint.create(engine, {
-    mouse: Mouse.create(render.canvas),
-  })
-);
+// World.add(
+//   world,
+//   MouseConstraint.create(engine, {
+//     mouse: Mouse.create(render.canvas),
+//   })
+// );
 
 //Walls
 const walls = [
-  Bodies.rectangle(400, 0, 800, 40, { isStatic: true }),
-  Bodies.rectangle(400, 600, 800, 40, { isStatic: true }),
-  Bodies.rectangle(0, 300, 40, 600, { isStatic: true }),
-  Bodies.rectangle(800, 300, 40, 600, { isStatic: true }),
+  Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }),
+  Bodies.rectangle(width / 2, height, width, 40, { isStatic: true }),
+  Bodies.rectangle(0, height / 2, 40, height, { isStatic: true }),
+  Bodies.rectangle(width, height / 2, 40, height, { isStatic: true }),
 ];
 World.add(world, walls);
-
-//Random Shapes
-for (let i = 0; i < 50; i++) {
-  if (Math.random() > 0.5) {
-    World.add(
-      world,
-      Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50)
-    );
-  } else {
-    World.add(
-      world,
-      Bodies.circle(Math.random() * width, Math.random() * height, 35, {
-        render: {
-          fillStyle: "green",
-        },
-      })
-    );
-  }
-}
 
 // Old Code
 //const shape = Bodies.rectangle(200, 200, 50, 50, {
 //   isStatic: true,
 // });
 // World.add(world, shape);
+//Random Shapes
+// for (let i = 0; i < 50; i++) {
+//   if (Math.random() > 0.5) {
+//     World.add(
+//       world,
+//       Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50)
+//     );
+//   } else {
+//     World.add(
+//       world,
+//       Bodies.circle(Math.random() * width, Math.random() * height, 35, {
+//         render: {
+//           fillStyle: "green",
+//         },
+//       })
+//     );
+//   }
+// }
