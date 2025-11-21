b4834216cb2d66843cbd689b6390254494320d5b
itssumitshukla <itssumitshukla@gmail.com>
2025-11-20 20:32:24 -1000

    setup encrypted parameter

--- javascriptProjects/message/script.js
+++ javascriptProjects/message/script.js
@@ -1,5 +1,6 @@
 document.querySelector("form").addEventListener("submit", (event) => {
   event.preventDefault();
-  const input = document.querySelector("input");
-  console.log(input.value);
+  const input = document.querySelector("#message-input");
+  const encrypted = btoa(input.value);
+  console.log(encrypted);
 });
