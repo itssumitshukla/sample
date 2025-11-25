56a5d40a297c91df6164ba858ba291b9d185019a
itssumitshukla <itssumitshukla@gmail.com>
2025-11-24 22:47:33 -1000

    toggle h1 to show after message is appened to the url

--- javascriptProjects/message/script.js
+++ javascriptProjects/message/script.js
@@ -6,6 +6,9 @@ const message = atob(hash.replace("#", " "));
 
 if (message) {
   document.querySelector("message-form").classList.add("hide");
+  document.querySelector("#message-show").classList.remove("hide");
+
+  document.querySelector("h1").innerHTML = message;
 }
 
 document.querySelector("form").addEventListener("submit", (event) => {
