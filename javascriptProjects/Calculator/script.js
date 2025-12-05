d8c75e0587e2c679d140ea53aa00653ff54f15f1
itssumitshukla <itssumitshukla@gmail.com>
2025-12-04 15:25:51 -1000

    setup decimal value catch

--- javascriptProjects/Calculator/script.js
+++ javascriptProjects/Calculator/script.js
@@ -9,6 +9,13 @@ function sendNumberValue(number) {
     displayValue === "0" ? number : displayValue + number;
 }
 
+function addDecimal() {
+  //If no decimal, add one
+  if (!calculatorDisplay.textContent.includes(".")) {
+    calculatorDisplay.textContent = `${calculatorDisplay.textContent}`;
+  }
+}
+
 //Add event listeners for numbers, operators, decimal
 inputBtns.forEach((inputBtn) => {
   if (inputBtn.classList.length === 0) {
