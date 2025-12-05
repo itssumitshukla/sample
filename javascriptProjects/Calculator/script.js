1ae1bc4a81108e818bd5f8fc9eafdc7b07fb83fe
itssumitshukla <itssumitshukla@gmail.com>
2025-12-04 15:20:48 -1000

    setup clear reset function

--- javascriptProjects/Calculator/script.js
+++ javascriptProjects/Calculator/script.js
@@ -19,3 +19,10 @@ inputBtns.forEach((inputBtn) => {
     inputBtn.addEventListener("click", () => sendNumberValue(inputBtn.value));
   }
 });
+
+//Reset display
+function resetAll() {
+  calculatorDisplay.textContent = "0";
+}
+
+//Event listener
