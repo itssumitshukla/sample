d877357fa0fba601a3227042335a3faa5c85ea58
itssumitshukla <itssumitshukla@gmail.com>
2025-12-10 17:28:14 -1000

    setup each button to re render the orignal background

--- javascriptProjects/Splash Page/script.js
+++ javascriptProjects/Splash Page/script.js
@@ -1,18 +1,27 @@
 const { body } = document;
 
 function changeBackground(number) {
+  // Check if background is already showing
+  let previousBackground;
+  if (body.className) {
+    previousBackground = body.className;
+    console.log(previousBackground);
+  }
   // Rest css body class
   body.className = "";
   switch (number) {
     case "1":
-      body.classList.add("background-1");
-      break;
+      return previousBackground === "background-1"
+        ? false
+        : body.classList.add("background-1");
     case "2":
-      body.classList.add("background-2");
-      break;
+      return previousBackground === "background-2"
+        ? false
+        : body.classList.add("background-2");
     case "3":
-      body.classList.add("background-3");
-      break;
+      return previousBackground === "background-3"
+        ? false
+        : body.classList.add("background-3");
     default:
       break;
   }
