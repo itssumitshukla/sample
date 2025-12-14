d14475a65fcb7420d2b5c935753380b8337f6345
itssumitshukla <itssumitshukla@gmail.com>
2025-12-14 12:19:23 -1000

    add logic to slider user input value

--- javascriptProjects/Paint Clone/script.js
+++ javascriptProjects/Paint Clone/script.js
@@ -22,14 +22,19 @@ let isEraser = false;
 // let drawnArray = [];
 
 // Formatting Brush Size
-// function displayBrushSize() {
-
-// }
+function displayBrushSize() {
+  if (brushSlider.value < 10) {
+    brushSize.textContent = `0${brushSlider.value}`;
+  } else {
+    brushSize.textContent = brushSlider.value;
+  }
+}
 
 // Setting Brush Size
-// brushSlider.addEventListener('change', () => {
-
-// });
+brushSlider.addEventListener("change", () => {
+  currentSize = brushSlider.value;
+  displayBrushSize();
+});
 
 // Setting Brush Color
 brushColorBtn.addEventListener("change", () => {
