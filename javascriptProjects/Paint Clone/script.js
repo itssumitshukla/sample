8444939f0ac3a3f0e693f01886a58cd19c9f47ae
itssumitshukla <itssumitshukla@gmail.com>
2025-12-15 15:41:26 -1000

    setup clear canvas function

--- javascriptProjects/Paint Clone/script.js
+++ javascriptProjects/Paint Clone/script.js
@@ -82,14 +82,14 @@ function createCanvas() {
   switchToBrush();
 }
 
-// // Clear Canvas
-// clearCanvasBtn.addEventListener('click', () => {
-//   createCanvas();
-//   drawnArray = [];
-//   // Active Tool
-//   activeToolEl.textContent = 'Canvas Cleared';
-//   setTimeout(switchToBrush, 1500);
-// });
+// Clear Canvas
+clearCanvasBtn.addEventListener("click", () => {
+  createCanvas();
+  drawnArray = [];
+  // Active Tool
+  activeToolEl.textContent = "Canvas Cleared";
+  setTimeout(switchToBrush, 1500);
+});
 
 // Draw what is stored in DrawnArray
 function restoreCanvas() {
