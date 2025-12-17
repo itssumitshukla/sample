7546145030d3acb5d6a409e531b966d3a59cefe8
itssumitshukla <itssumitshukla@gmail.com>
2025-12-16 16:51:59 -1000

    setup js paddle,body parameter

---
+++ javascriptProjects/Pong Game/script.js
@@ -0,0 +1,29 @@
+// Canvas
+const { body } = document;
+
+const width = 500;
+const height = 700;
+const screenWidth = window.screen.width;
+const canvasPosition = screenWidth / 2 - width / 2;
+const isMobile = window.matchMedia("(max-width: 600px)");
+const gameOverEl = document.createElement("div");
+
+// Paddle
+const paddleHeight = 10;
+const paddleWidth = 50;
+const paddleDiff = 25;
+let paddleBottomX = 225;
+let paddleTopX = 225;
+let playerMoved = false;
+let paddleContact = false;
+
+// Ball
+let ballX = 250;
+let ballY = 350;
+const ballRadius = 5;
+
+// Speed
+let speedY;
+let speedX;
+let trajectoryX;
+let computerSpeed;
