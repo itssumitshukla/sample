e31f62418dc7a00e5e341fb8a45fcac1294abdb8
itssumitshukla <itssumitshukla@gmail.com>
2025-12-17 19:33:34 -1000

    setup paddle for user and computer

--- javascriptProjects/Pong Game/script.js
+++ javascriptProjects/Pong Game/script.js
@@ -53,11 +53,11 @@ function renderCanvas() {
   context.fillStyle = "black";
   context.fillRect(0, 0, width, height);
   //   Paddle Color
-  //   context.fillStyle = 'white';
+  context.fillStyle = "white";
   //   Player Paddle (Bottom)
-  //   context.fillRect(paddleBottomX, height - 20, paddleWidth, paddleHeight);
+  context.fillRect(paddleBottomX, height - 20, paddleWidth, paddleHeight);
   //   Computer Paddle (Top)
-  //   context.fillRect(paddleTopX, 10, paddleWidth, paddleHeight);
+  context.fillRect(paddleTopX, 10, paddleWidth, paddleHeight);
   //   Dashed Center Line
   //   context.beginPath();
   //   context.setLineDash([4]);
