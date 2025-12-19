d1e7ebbfbff1996db18ff0c2c33bd4bbd70614ab
itssumitshukla <itssumitshukla@gmail.com>
2025-12-18 16:35:51 -1000

    setup game over append function

--- javascriptProjects/Pong Game/script.js
+++ javascriptProjects/Pong Game/script.js
@@ -167,6 +167,7 @@ function computerAI() {
 
 function showGameOverEl(winner) {
   // Hide Canvas
+  canvas.hidden = "true";
   // Container
   gameOverEl.textContent = "";
   gameOverEl.classList.add("game-over-container");
@@ -178,6 +179,8 @@ function showGameOverEl(winner) {
   playAgainBtn.setAttribute("onclick", "startGame()");
   playAgainBtn.textContent = "Play Again";
   // Append
+  gameOverEl.append(title, playAgainBtn);
+  body.appendChild(gameOver);
 }
 
 // Check If One Player Has Winning Score, If They Do, End Game
