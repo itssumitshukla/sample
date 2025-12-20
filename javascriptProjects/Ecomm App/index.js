b8e2498c4b3e15458e8661933e0affaa4eddab2f
itssumitshukla <itssumitshukla@gmail.com>
2025-12-20 13:21:22 -1000

    test form rendering using express

--- javascriptProjects/Ecomm App/index.js
+++ javascriptProjects/Ecomm App/index.js
@@ -3,7 +3,16 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("hi there");
+  res.send(`
+    <div>
+        <form>
+            <input placeholder="email" />
+            <input placeholder="password" />
+            <input placeholder="password confirmation" />
+            <button>SignUP</button>
+        </form>
+    </div>
+    `);
 });
 
 app.listen(3000, () => {
