043f8189c466c4bbecbac99ca3f4ee88c3107391
itssumitshukla <itssumitshukla@gmail.com>
2025-12-21 19:43:00 -1000

    test req method in parser and in app.post

--- javascriptProjects/Ecomm App/index.js
+++ javascriptProjects/Ecomm App/index.js
@@ -16,18 +16,21 @@ app.get("/", (req, res) => {
 });
 
 const bodyParser = (req, res, next) => {
-  req.on("data", (data) => {
-    const parsed = data.toString("utf8").split("&");
-    const formData = {};
-    for (let pair of parsed) {
-      const [key, value] = pair.split("=");
-      formData[key] = value;
-    }
-    console.log(formData);
-  });
+  if (req.method === "POST") {
+    req.on("data", (data) => {
+      const parsed = data.toString("utf8").split("&");
+      const formData = {};
+      for (let pair of parsed) {
+        const [key, value] = pair.split("=");
+        formData[key] = value;
+      }
+      console.log(formData);
+    });
+  }
 };
 
 app.post("/", (req, res) => {
+  console.log(req);
   res.send("Account created");
 });
 
