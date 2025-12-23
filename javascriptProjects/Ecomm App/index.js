12ed7bdba75fef00e0dfc97877c07e7073a2bbf0
itssumitshukla <itssumitshukla@gmail.com>
2025-12-22 14:23:08 -1000

    setup body parser globally

--- javascriptProjects/Ecomm App/index.js
+++ javascriptProjects/Ecomm App/index.js
@@ -3,6 +3,8 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.send(`
     <div>
@@ -33,7 +35,7 @@ app.get("/", (req, res) => {
 //   }
 // };
 
-app.post("/", bodyParser.urlencoded({ extended: true }), (req, res) => {
+app.post("/", (req, res) => {
   console.log(req.body);
   res.send("Account created");
 });
