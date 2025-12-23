a0a9493617ab336e8e6ab66bdfd333fc84490af8
itssumitshukla <itssumitshukla@gmail.com>
2025-12-23 12:27:54 -1000

    test node js files access sync

--- javascriptProjects/Ecomm App/repositories/users.js
+++ javascriptProjects/Ecomm App/repositories/users.js
@@ -1,7 +1,16 @@
+const fs = require("fs");
+
 class UsersRepository {
   constructor(filename) {
     if (!filename) {
       throw new Error("Create a repository requires a filname");
     }
+
+    this.filename = filename;
+    try {
+      fs.accessSync(this.filename);
+    } catch (error) {}
   }
 }
+
+new UsersRepository();
