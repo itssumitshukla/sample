3a3f70777943dcae880bc8cd8918d7fb20fcd0a4
itssumitshukla <itssumitshukla@gmail.com>
2025-12-24 18:39:55 -1000

    test get one user id using the await and crpyptp function

--- javascriptProjects/Ecomm App/repositories/users.js
+++ javascriptProjects/Ecomm App/repositories/users.js
@@ -41,17 +41,25 @@ class UsersRepository {
   randomId() {
     return crypto.randomBytes(4).toString("hex");
   }
+
+  async getOne(id) {
+    const records = await this.getAll();
+    return records.find((records) => records.id === id);
+  }
 }
 
 const test = async () => {
   const repo = new UsersRepository("users.json");
-  await repo.create({
-    email: "test@test.com",
-    password: "password",
-  });
+  //   await repo.create({
+  //     email: "test@test.com",
+  //     password: "password",
+  //   });
 
-  const users = await repo.getAll();
-  console.log(users);
+  //   const users = await repo.getAll();
+
+  const user = await repo.getOne("772a6480");
+
+  console.log(user);
 };
 
 test();
