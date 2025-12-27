2819fad4fcac7fc20a78f5fc51919f3e0596044f
itssumitshukla <itssumitshukla@gmail.com>
2025-12-26 18:10:55 -1000

    setup signin route and add form

--- javascriptProjects/Ecomm App/index.js
+++ javascriptProjects/Ecomm App/index.js
@@ -67,6 +67,18 @@ app.get("/signout", (req, res) => {
   req.send("You are logged out");
 });
 
+app.get("/signin", (req, res) => {
+  res.send(`
+            <div>
+        <form method="POST">
+            <input name="email" placeholder="email" />
+            <input name="password" placeholder="password" />
+            <button>Sign In</button>
+        </form>
+    </div>
+        `);
+});
+
 app.listen(3000, () => {
   console.log("Listening");
 });
