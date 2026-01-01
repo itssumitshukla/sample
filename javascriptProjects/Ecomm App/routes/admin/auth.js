9b1d826b86aff6f79e9fa4a7f35b176167073700
itssumitshukla <itssumitshukla@gmail.com>
2025-12-31 20:29:38 -1000

    setup signup html template

--- javascriptProjects/Ecomm App/routes/admin/auth.js
+++ javascriptProjects/Ecomm App/routes/admin/auth.js
@@ -1,20 +1,11 @@
 const express = require("express");
 const usersRepo = require("../../repositories/users");
+const signupTemplate = require("../../views/admin/auth/signup");
 
 const router = express.Router();
 
 router.get("/signup", (req, res) => {
-  res.send(`
-    <div>
-    Your Id is: ${req.session.userId}
-        <form method="POST">
-            <input name="email" placeholder="email" />
-            <input name="password" placeholder="password" />
-            <input name="passwordConfirmation" placeholder="password confirmation" />
-            <button>SignUP</button>
-        </form>
-    </div>
-    `);
+  res.send(signupTemplate({ req }));
 });
 
 // const bodyParser = (req, res, next) => {
