9a98c508d7afc3781a18aaea02c88211ac4dfc95
itssumitshukla <itssumitshukla@gmail.com>
2026-01-02 20:29:28 -1000

    setup check validation in auth js file

--- javascriptProjects/Ecomm App/routes/admin/auth.js
+++ javascriptProjects/Ecomm App/routes/admin/auth.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { check } = require("express-validator");
 const usersRepo = require("../../repositories/users");
 const signupTemplate = require("../../views/admin/auth/signup");
 const signinTemplate = require("../../views/admin/auth/signin");
@@ -26,24 +27,28 @@ router.get("/signup", (req, res) => {
 //   }
 // };
 
-router.post("/signup", async (req, res) => {
-  const { email, password, passwordConfirmation } = req.body;
-  const existingUser = await usersRepo.getOneBy({ email });
-  if (existingUser) {
-    return res.send("Email in use");
+router.post(
+  "/signup",
+  [check("email").isEmail(), check("password"), check("passwordConfirmation")],
+  async (req, res) => {
+    const { email, password, passwordConfirmation } = req.body;
+    const existingUser = await usersRepo.getOneBy({ email });
+    if (existingUser) {
+      return res.send("Email in use");
+    }
+    if (password !== passwordConfirmation) {
+      return res.send("Password do not match");
+    }
+
+    //create a user in our user repo
+    const user = await usersRepo.create({ email, password });
+
+    //store the id of that user inside the users cookie
+    req.session.userId = user.id;
+
+    res.send("Account created");
   }
-  if (password !== passwordConfirmation) {
-    return res.send("Password do not match");
-  }
-
-  //create a user in our user repo
-  const user = await usersRepo.create({ email, password });
-
-  //store the id of that user inside the users cookie
-  req.session.userId = user.id;
-
-  res.send("Account created");
-});
+);
 
 app.get("/signout", (req, res) => {
   req.session = null;
