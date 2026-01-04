1d388cfd18a4f490c38e3b3a30263151b109733b
itssumitshukla <itssumitshukla@gmail.com>
2026-01-03 14:53:54 -1000

    setup middleware js file

--- javascriptProjects/Ecomm App/index.js
+++ javascriptProjects/Ecomm App/index.js
@@ -2,9 +2,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cookieSession = require("cookie-session");
 const authRouter = require("./routes/admin/auth");
-const adminProductsRouter = require("./routes/admin/products");
-const productsRouter = require("./routes/products");
-const cartsRouter = require("./routes/carts");
+//const adminProductsRouter = require("./routes/admin/products");
+//const productsRouter = require("./routes/products");
+//const cartsRouter = require("./routes/carts");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,9 +15,6 @@ app.use(
 );
 
 app.use(authRouter);
-app.use(productsRouter);
-app.use(adminProductsRouter);
-app.use(cartsRouter);
 
 app.listen(3000, () => {
   console.log("Listening");
