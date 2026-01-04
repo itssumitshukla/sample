e98013c74b4626c50ab0f8d43ab296964996f71a
itssumitshukla <itssumitshukla@gmail.com>
2026-01-03 14:52:06 -1000

    setup require for admin product and carts router

--- javascriptProjects/Ecomm App/index.js
+++ javascriptProjects/Ecomm App/index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cookieSession = require("cookie-session");
 const authRouter = require("./routes/admin/auth");
+const adminProductsRouter = require("./routes/admin/products");
+const productsRouter = require("./routes/products");
+const cartsRouter = require("./routes/carts");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -12,6 +15,9 @@ app.use(
 );
 
 app.use(authRouter);
+app.use(productsRouter);
+app.use(adminProductsRouter);
+app.use(cartsRouter);
 
 app.listen(3000, () => {
   console.log("Listening");
