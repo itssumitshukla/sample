8021fc8a65e950b2ac04aeebbf11b869e221f304
itssumitshukla <itssumitshukla@gmail.com>
2026-01-03 14:36:36 -1000

    setup custom existing user error check

--- javascriptProjects/Ecomm App/routes/admin/auth.js
+++ javascriptProjects/Ecomm App/routes/admin/auth.js
@@ -30,7 +30,17 @@ router.get("/signup", (req, res) => {
 router.post(
   "/signup",
   [
-    check("email").trim().normalizeEmail().isEmail(),
+    check("email")
+      .trim()
+      .normalizeEmail()
+      .isEmail()
+      .withMessage("Must be a valid email")
+      .custom(async (email) => {
+        const existingUser = await usersRepo.getOneBy({ email });
+        if (existingUser) {
+          throw new Error("Email in use");
+        }
+      }),
     check("password").trim().isLength({ min: 4, max: 20 }),
     check("passwordConfirmation").trim().isLength({ min: 4, max: 20 }),
   ],
@@ -38,10 +48,7 @@ router.post(
     const errors = validationResult(req);
     console.log(errors);
     const { email, password, passwordConfirmation } = req.body;
-    const existingUser = await usersRepo.getOneBy({ email });
-    if (existingUser) {
-      return res.send("Email in use");
-    }
+
     if (password !== passwordConfirmation) {
       return res.send("Password do not match");
     }
