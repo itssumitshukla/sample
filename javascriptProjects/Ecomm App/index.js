8c6320256ee435a21e50ce41ef957a20d53c7261
itssumitshukla <itssumitshukla@gmail.com>
2026-01-07 14:49:27 -1000

    update index.js with cart router

--- javascriptProjects/Ecomm App/index.js
+++ javascriptProjects/Ecomm App/index.js
@@ -3,20 +3,22 @@ const bodyParser = require("body-parser");
 const cookieSession = require("cookie-session");
 const authRouter = require("./routes/admin/auth");
 const adminProductsRouter = require("./routes/admin/products");
-const productsRouter = require("./routes/");
-//const cartsRouter = require("./routes/carts");
+const productsRouter = require("./routes/products");
+const cartsRouter = require("./routes/carts");
+
 const app = express();
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cookieSession({
-    keys: ["asdgfreeewag"],
+    keys: ["lkasld235j"],
   })
 );
-
 app.use(authRouter);
 app.use(productsRouter);
+app.use(adminProductsRouter);
+app.use(cartsRouter);
 
 app.listen(3000, () => {
   console.log("Listening");
