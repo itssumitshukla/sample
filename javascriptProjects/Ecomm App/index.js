5de5b35dc74cc3739f9a5ea36437ae2296381c0e
itssumitshukla <itssumitshukla@gmail.com>
2026-01-07 15:18:22 -1000

    fix cartjs error

--- javascriptProjects/Ecomm App/index.js
+++ javascriptProjects/Ecomm App/index.js
@@ -4,7 +4,7 @@ const cookieSession = require("cookie-session");
 const authRouter = require("./routes/admin/auth");
 const adminProductsRouter = require("./routes/admin/products");
 const productsRouter = require("./routes/products");
-const cartsRouter = require("./routes/cart");
+const cartsRouter = require("./routes/carts");
 
 const app = express();
 
