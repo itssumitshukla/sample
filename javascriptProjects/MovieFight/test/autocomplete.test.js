7193ffaf3d56cfa91a2d11bd48cdffe1aa6caca8
itssumitshukla <itssumitshukla@gmail.com>
2026-01-11 16:28:05 -1000

    setup input mocha test parameter

--- javascriptProjects/MovieFight/test/autocomplete.test.js
+++ javascriptProjects/MovieFight/test/autocomplete.test.js
@@ -1,5 +1,11 @@
-it("Shows an autocomplete", () => {
-  createAutoComplete({
-    root: document.querySelector("#target"),
+describe("Authentication feature", () => {
+  it("Test input", () => {
+    console.log("TEST");
+  });
+
+  it("Shows an autocomplete", () => {
+    createAutoComplete({
+      root: document.querySelector("#target"),
+    });
   });
 });
