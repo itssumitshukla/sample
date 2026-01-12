b520f019b9926eae05e37023f44158237a906e57
itssumitshukla <itssumitshukla@gmail.com>
2026-01-11 16:53:49 -1000

    setup promise and timeout for input test

--- javascriptProjects/MovieFight/test/autocomplete.test.js
+++ javascriptProjects/MovieFight/test/autocomplete.test.js
@@ -1,11 +1,61 @@
-describe("Authentication feature", () => {
-  it("Test input", () => {
-    console.log("TEST");
-  });
+// describe("Authentication feature", () => {
+//   it("Test input", () => {
+//     console.log("TEST");
+//   });
 
-  it("Shows an autocomplete", () => {
-    createAutoComplete({
-      root: document.querySelector("#target"),
-    });
+//   it("Shows an autocomplete", () => {
+//     createAutoComplete({
+//       root: document.querySelector("#target"),
+//     });
+//   });
+// });
+
+const waitFor = (selector) => {
+  return new Promise((resolve, reject) => {
+    const interval = setInterval(() => {
+      if (document.querySelector(selector)) {
+        clearInterval(interval);
+        clearTimeout(timeout);
+        resolve();
+      }
+    }, 30);
+    const timeout = setTimeout(() => {
+      clearInterval(interval);
+      reject();
+    }, 3500);
   });
+};
+
+beforeEach(() => {
+  document.querySelector("#target").innerHTML = "";
+  createAutoComplete({
+    root: document.querySelector("#target"),
+    fetchData() {
+      return [
+        { Title: "avengers" },
+        { Title: "not avengers" },
+        { Title: "movies" },
+      ];
+    },
+    renderOption(movie) {
+      return movie.Title;
+    },
+  });
+});
+
+it("dropdown starts closed", () => {
+  const dropdown = document.querySelector(".dropdown");
+
+  expect(dropdown.className).not.to.include("is-active");
+});
+
+it("after search, dropdown opens up", async () => {
+  const input = document.querySelector("input");
+  input.value = "avengers";
+  input.dispatchEvent(new Event("input"));
+
+  await waitFor(".dropdown-item");
+
+  const dropdown = document.querySelector(".dropdown");
+  expect(dropdown.className).to.include("is-active");
 });
