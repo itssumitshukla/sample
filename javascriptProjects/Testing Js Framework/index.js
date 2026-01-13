75bfad6faec01bd53811e9ff1435176b25a29769
itssumitshukla <itssumitshukla@gmail.com>
2026-01-12 22:12:08 -1000

    setup runner in index file

--- javascriptProjects/Testing Js Framework/index.js
+++ javascriptProjects/Testing Js Framework/index.js
@@ -1,3 +1,12 @@
 #!usr/bin/env node
 const Runner = require("./runner");
+const runner = new Runner();
+
+const run = async () => {
+  const results = await runner.collectFiles;
+  console.log(results);
+};
+
+run();
+
 console.log("Running TEST");
