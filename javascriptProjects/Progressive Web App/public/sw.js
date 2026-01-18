2bc17c8d9b940010826347620b922512b2474b10
itssumitshukla <itssumitshukla@gmail.com>
2026-01-18 12:24:29 -1000

    fix service worker event error

--- javascriptProjects/Progressive Web App/public/sw.js
+++ javascriptProjects/Progressive Web App/public/sw.js
@@ -1,7 +1,10 @@
 self.addEventListener("install", function (event) {
-  console.log("Serivce working[SW file] Installing Service worker...".event);
+  console.log("Serivce working[SW file] Installing Service worker...", event);
 });
 self.addEventListener("activate", function (event) {
-  console.log("Serivce working[SW file] Activating Service worker...".event);
+  console.log("Serivce working[SW file] Activating Service worker...", event);
   return self.clients.claim();
+});
+self.addEventListener("fetch", function (event) {
+  console.log("Serivce working[SW file] Fetching Something...", event);
 });
