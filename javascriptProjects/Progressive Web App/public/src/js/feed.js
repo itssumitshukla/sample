510cab85acd09b543b7e6b2140c68b62196d9a88
itssumitshukla <itssumitshukla@gmail.com>
2026-01-20 10:48:32 -1000

    setup feed js

--- javascriptProjects/Progressive Web App/public/src/js/feed.js
+++ javascriptProjects/Progressive Web App/public/src/js/feed.js
@@ -3,19 +3,24 @@ var createPostArea = document.querySelector("#create-post");
 var closeCreatePostModalButton = document.querySelector(
   "#close-create-post-modal-btn",
 );
+var sharedMomentsArea = document.querySelector("#shared-moments");
 
 function openCreatePostModal() {
   createPostArea.style.display = "block";
   if (deferredPrompt) {
-    deferredPrompt.promp();
-    deferredPrompt.userChoise.then(function (choiceResult) {
+    deferredPrompt.prompt();
+
+    deferredPrompt.userChoice.then(function (choiceResult) {
       console.log(choiceResult.outcome);
+
       if (choiceResult.outcome === "dismissed") {
         console.log("User cancelled installation");
       } else {
         console.log("User added to home screen");
       }
     });
+
+    deferredPrompt = null;
   }
 }
 
@@ -26,3 +31,33 @@ function closeCreatePostModal() {
 shareImageButton.addEventListener("click", openCreatePostModal);
 
 closeCreatePostModalButton.addEventListener("click", closeCreatePostModal);
+
+function createCard() {
+  var cardWrapper = document.createElement("div");
+  cardWrapper.className = "shared-moment-card mdl-card mdl-shadow--2dp";
+  var cardTitle = document.createElement("div");
+  cardTitle.className = "mdl-card__title";
+  cardTitle.style.backgroundImage = 'url("/src/images/sf-boat.jpg")';
+  cardTitle.style.backgroundSize = "cover";
+  cardTitle.style.height = "180px";
+  cardWrapper.appendChild(cardTitle);
+  var cardTitleTextElement = document.createElement("h2");
+  cardTitleTextElement.className = "mdl-card__title-text";
+  cardTitleTextElement.textContent = "San Francisco Trip";
+  cardTitle.appendChild(cardTitleTextElement);
+  var cardSupportingText = document.createElement("div");
+  cardSupportingText.className = "mdl-card__supporting-text";
+  cardSupportingText.textContent = "In San Francisco";
+  cardSupportingText.style.textAlign = "center";
+  cardWrapper.appendChild(cardSupportingText);
+  componentHandler.upgradeElement(cardWrapper);
+  sharedMomentsArea.appendChild(cardWrapper);
+}
+
+fetch("https://httpbin.org/get")
+  .then(function (res) {
+    return res.json();
+  })
+  .then(function (data) {
+    createCard();
+  });
