2361a13dbe5e7e137381c0d1a73cea393efd1eb1
itssumitshukla <itssumitshukla@gmail.com>
2026-01-20 15:20:17 -1000

    setup service worker with cache match

--- javascriptProjects/Progressive Web App/public/sw.js
+++ javascriptProjects/Progressive Web App/public/sw.js
@@ -14,5 +14,15 @@ self.addEventListener("activate", function (event) {
 });
 
 self.addEventListener("fetch", function (event) {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    event.respondWith(
+      caches.match(event.request).then(function (response) {
+        if (response) {
+          return response;
+        } else {
+          return fetch(event.request);
+        }
+      }),
+    ),
+  );
 });
