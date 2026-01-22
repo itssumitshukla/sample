ead4aff8fac17123b48bb63925a2f7f1e7b10412
itssumitshukla <itssumitshukla@gmail.com>
2026-01-21 15:04:18 -1000

    setup request url cache

--- javascriptProjects/Progressive Web App/public/sw.js
+++ javascriptProjects/Progressive Web App/public/sw.js
@@ -30,7 +30,11 @@ self.addEventListener("fetch", function (event) {
         if (response) {
           return response;
         } else {
-          return fetch(event.request);
+          return fetch(event.request).then(function (res) {
+            return caches.open("dynamic").then(function (cache) {
+              cache.put(event.request.url, res);
+            });
+          });
         }
       }),
     ),
