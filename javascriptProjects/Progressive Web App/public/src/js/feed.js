6a7e1f7e6e56302656d0c8e44965b855e9f109c3
itssumitshukla <itssumitshukla@gmail.com>
2026-01-21 20:32:54 -1000

    add user btn to save for offline use

--- javascriptProjects/Progressive Web App/public/src/js/feed.js
+++ javascriptProjects/Progressive Web App/public/src/js/feed.js
@@ -1,9 +1,9 @@
-var shareImageButton = document.querySelector("#share-image-button");
-var createPostArea = document.querySelector("#create-post");
-var closeCreatePostModalButton = document.querySelector(
+let shareImageButton = document.querySelector("#share-image-button");
+let createPostArea = document.querySelector("#create-post");
+let closeCreatePostModalButton = document.querySelector(
   "#close-create-post-modal-btn",
 );
-var sharedMomentsArea = document.querySelector("#shared-moments");
+let sharedMomentsArea = document.querySelector("#shared-moments");
 
 function openCreatePostModal() {
   createPostArea.style.display = "block";
@@ -33,22 +33,25 @@ shareImageButton.addEventListener("click", openCreatePostModal);
 closeCreatePostModalButton.addEventListener("click", closeCreatePostModal);
 
 function createCard() {
-  var cardWrapper = document.createElement("div");
+  let cardWrapper = document.createElement("div");
   cardWrapper.className = "shared-moment-card mdl-card mdl-shadow--2dp";
-  var cardTitle = document.createElement("div");
+  let cardTitle = document.createElement("div");
   cardTitle.className = "mdl-card__title";
   cardTitle.style.backgroundImage = 'url("/src/images/sf-boat.jpg")';
   cardTitle.style.backgroundSize = "cover";
   cardTitle.style.height = "180px";
   cardWrapper.appendChild(cardTitle);
-  var cardTitleTextElement = document.createElement("h2");
+  let cardTitleTextElement = document.createElement("h2");
   cardTitleTextElement.className = "mdl-card__title-text";
   cardTitleTextElement.textContent = "San Francisco Trip";
   cardTitle.appendChild(cardTitleTextElement);
-  var cardSupportingText = document.createElement("div");
+  let cardSupportingText = document.createElement("div");
   cardSupportingText.className = "mdl-card__supporting-text";
   cardSupportingText.textContent = "In San Francisco";
   cardSupportingText.style.textAlign = "center";
+  let cardSaveButton = document.createElement("button");
+  cardSaveButton.textContent = "save";
+  cardSupportingText.appendChild(cardSaveButton);
   cardWrapper.appendChild(cardSupportingText);
   componentHandler.upgradeElement(cardWrapper);
   sharedMomentsArea.appendChild(cardWrapper);
