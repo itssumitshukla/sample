e2c8874334c40772b82c7481b38e1b3000dc9d54
itssumitshukla <itssumitshukla@gmail.com>
2026-01-22 15:35:05 -1000

    update feed for network throttle

--- javascriptProjects/Progressive Web App/public/src/js/feed.js
+++ javascriptProjects/Progressive Web App/public/src/js/feed.js
@@ -22,6 +22,15 @@ function openCreatePostModal() {
 
     deferredPrompt = null;
   }
+
+  // if ('serviceWorker' in navigator) {
+  //   navigator.serviceWorker.getRegistrations()
+  //     .then(function(registrations) {
+  //       for (let i = 0; i < registrations.length; i++) {
+  //         registrations[i].unregister();
+  //       }
+  //     })
+  // }
 }
 
 function closeCreatePostModal() {
@@ -32,15 +41,22 @@ shareImageButton.addEventListener("click", openCreatePostModal);
 
 closeCreatePostModalButton.addEventListener("click", closeCreatePostModal);
 
-// function onSaveButtonClicked(event) {
-//   console.log("clicked");
-//   if ("caches" in window) {
-//     caches.open("user-requested").then(function (cache) {
-//       cache.add("https://httpbin.org/get");
-//       cache.add("/public/src/images/sf-boat.jpg");
-//     });
-//   }
-// }
+// Currently not in use, allows to save assets in cache on demand otherwise
+function onSaveButtonClicked(event) {
+  console.log("clicked");
+  if ("caches" in window) {
+    caches.open("user-requested").then(function (cache) {
+      cache.add("https://httpbin.org/get");
+      cache.add("/src/images/sf-boat.jpg");
+    });
+  }
+}
+
+function clearCards() {
+  while (sharedMomentsArea.hasChildNodes()) {
+    sharedMomentsArea.removeChild(sharedMomentsArea.lastChild);
+  }
+}
 
 function createCard() {
   let cardWrapper = document.createElement("div");
@@ -52,6 +68,7 @@ function createCard() {
   cardTitle.style.height = "180px";
   cardWrapper.appendChild(cardTitle);
   let cardTitleTextElement = document.createElement("h2");
+  cardTitleTextElement.style.color = "white";
   cardTitleTextElement.className = "mdl-card__title-text";
   cardTitleTextElement.textContent = "San Francisco Trip";
   cardTitle.appendChild(cardTitleTextElement);
@@ -59,19 +76,51 @@ function createCard() {
   cardSupportingText.className = "mdl-card__supporting-text";
   cardSupportingText.textContent = "In San Francisco";
   cardSupportingText.style.textAlign = "center";
-  // let cardSaveButton = document.createElement("button");
-  // cardSaveButton.textContent = "save";
-  // cardSaveButton.addEventListener("click", onSaveButtonClicked);
+  // let cardSaveButton = document.createElement('button');
+  // cardSaveButton.textContent = 'Save';
+  // cardSaveButton.addEventListener('click', onSaveButtonClicked);
   // cardSupportingText.appendChild(cardSaveButton);
   cardWrapper.appendChild(cardSupportingText);
   componentHandler.upgradeElement(cardWrapper);
   sharedMomentsArea.appendChild(cardWrapper);
 }
 
-fetch("https://httpbin.org/get")
+let url = "https://httpbin.org/post";
+let networkDataReceived = false;
+
+fetch(url, {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+  body: JSON.stringify({
+    message: "Some message",
+  }),
+})
   .then(function (res) {
     return res.json();
   })
   .then(function (data) {
+    networkDataReceived = true;
+    console.log("From web", data);
+    clearCards();
     createCard();
   });
+
+if ("caches" in window) {
+  caches
+    .match(url)
+    .then(function (response) {
+      if (response) {
+        return response.json();
+      }
+    })
+    .then(function (data) {
+      console.log("From cache", data);
+      if (!networkDataReceived) {
+        clearCards();
+        createCard();
+      }
+    });
+}
