728eeb2efa6f1be152d3902e8770585adaa6a4b7
itssumitshukla <itssumitshukla@gmail.com>
2026-01-24 13:05:39 -1000

    update sw file for db import

--- javascriptProjects/Progressive Web App/public/sw.js
+++ javascriptProjects/Progressive Web App/public/sw.js
@@ -1,7 +1,7 @@
 importScripts("/src/js/idb.js");
 importScripts("/src/js/utility.js");
 
-let CACHE_STATIC_NAME = "static-v16";
+let CACHE_STATIC_NAME = "static-v18";
 let CACHE_DYNAMIC_NAME = "dynamic-v2";
 let STATIC_FILES = [
   "/",
@@ -79,11 +79,15 @@ self.addEventListener("fetch", function (event) {
     event.respondWith(
       fetch(event.request).then(function (res) {
         let clonedRes = res.clone();
-        clonedRes.json().then(function (data) {
-          for (let key in data) {
-            writeData("posts", data[key]);
-          }
-        });
+        clearAllData("posts")
+          .then(function () {
+            return clonedRes.json();
+          })
+          .then(function (data) {
+            for (let key in data) {
+              writeData("posts", data[key]);
+            }
+          });
         return res;
       }),
     );
