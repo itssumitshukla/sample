0f9c4fc5e3840dbfff0cc6a157798dd91a35af48
itssumitshukla <itssumitshukla@gmail.com>
2026-01-26 15:49:41 -1000

    update utility js with updated function

--- javascriptProjects/Progressive Web App/public/src/js/utility.js
+++ javascriptProjects/Progressive Web App/public/src/js/utility.js
@@ -1,13 +1,16 @@
-let dbPromise = idb.open("posts-store", 1, function (db) {
+var dbPromise = idb.open("posts-store", 1, function (db) {
   if (!db.objectStoreNames.contains("posts")) {
     db.createObjectStore("posts", { keyPath: "id" });
+  }
+  if (!db.objectStoreNames.contains("sync-posts")) {
+    db.createObjectStore("sync-posts", { keyPath: "id" });
   }
 });
 
 function writeData(st, data) {
   return dbPromise.then(function (db) {
-    let tx = db.transaction(st, "readwrite");
-    let store = tx.objectStore(st);
+    var tx = db.transaction(st, "readwrite");
+    var store = tx.objectStore(st);
     store.put(data);
     return tx.complete;
   });
@@ -15,16 +18,16 @@ function writeData(st, data) {
 
 function readAllData(st) {
   return dbPromise.then(function (db) {
-    let tx = db.transaction(st, "readonly");
-    let store = tx.objectStore(st);
+    var tx = db.transaction(st, "readonly");
+    var store = tx.objectStore(st);
     return store.getAll();
   });
 }
 
 function clearAllData(st) {
   return dbPromise.then(function (db) {
-    let tx = db.transaction(st, "readwrite");
-    let store = tx.objectStore(st);
+    var tx = db.transaction(st, "readwrite");
+    var store = tx.objectStore(st);
     store.clear();
     return tx.complete;
   });
@@ -33,8 +36,8 @@ function clearAllData(st) {
 function deleteItemFromData(st, id) {
   dbPromise
     .then(function (db) {
-      let tx = db.transaction(st, "readwrite");
-      let store = tx.objectStore(st);
+      var tx = db.transaction(st, "readwrite");
+      var store = tx.objectStore(st);
       store.delete(id);
       return tx.complete;
     })
