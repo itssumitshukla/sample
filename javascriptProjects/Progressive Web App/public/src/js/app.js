ca8fe838510aa05479c2725b142c64d90d083ef2
itssumitshukla <itssumitshukla@gmail.com>
2026-01-26 15:38:06 -1000

    setup fetch from firebase

--- javascriptProjects/Progressive Web App/public/src/js/app.js
+++ javascriptProjects/Progressive Web App/public/src/js/app.js
@@ -1,4 +1,7 @@
 let deferredPrompt;
+let enableNotificationsButtons = document.querySelectorAll(
+  ".enable-notifications",
+);
 
 if (!window.Promise) {
   window.Promise = Promise;
@@ -21,3 +24,31 @@ window.addEventListener("beforeinstallprompt", function (event) {
   deferredPrompt = event;
   return false;
 });
+
+function displayConfirmNotification() {
+  let options = {
+    body: "You successfully subscribed to our Notification service!",
+  };
+  new Notification("Successfully subscribed!", options);
+}
+
+function askForNotificationPermission() {
+  Notification.requestPermission(function (result) {
+    console.log("User Choice", result);
+    if (result !== "granted") {
+      console.log("No notification permission granted!");
+    } else {
+      displayConfirmNotification();
+    }
+  });
+}
+
+if ("Notification" in window) {
+  for (let i = 0; i < enableNotificationsButtons.length; i++) {
+    enableNotificationsButtons[i].style.display = "inline-block";
+    enableNotificationsButtons[i].addEventListener(
+      "click",
+      askForNotificationPermission,
+    );
+  }
+}
