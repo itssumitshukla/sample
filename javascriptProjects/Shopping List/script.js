da69b2b42e4806f2c4a9435bb0aa4799bd38b0d7
itssumitshukla <itssumitshukla@gmail.com>
2026-02-13 17:27:25 -1000

    fix icon append child

--- javascriptProjects/Shopping List/script.js
+++ javascriptProjects/Shopping List/script.js
@@ -20,7 +20,15 @@ function addItem(e) {
 function createButton(classes) {
   const button = document.createElement("button");
   button.className = classes;
+  const icon = createIcon("fa-solid fa-xmark");
+  button.appendChild(icon);
   return button;
+}
+
+function createIcon(classes) {
+  const icon = document.createElement("i");
+  icon.className = classes;
+  return icon;
 }
 
 itemForm.addEventListener("submit", addItem);
