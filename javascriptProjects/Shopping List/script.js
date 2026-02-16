bd9a9d4d7b8d15a2ba5f741b53801f8381bae36c
itssumitshukla <itssumitshukla@gmail.com>
2026-02-15 18:43:11 -1000

    setup confrim during remove item

--- javascriptProjects/Shopping List/script.js
+++ javascriptProjects/Shopping List/script.js
@@ -46,7 +46,9 @@ function createIcon(classes) {
 
 function removeItem(e) {
   if (e.target.parentElement.classList.contains("remove-item")) {
-    e.target.parentElement.parentElement.remove();
+    if (confirm("Are you sure")) {
+      e.target.parentElement.parentElement.remove();
+    }
   }
 }
 
