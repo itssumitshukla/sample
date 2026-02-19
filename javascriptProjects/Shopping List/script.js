c69e22693e7d16d0722899a55d8b88d0bdb3b2a9
itssumitshukla <itssumitshukla@gmail.com>
2026-02-18 15:05:25 -1000

    fix item to loop over and edit just the user change input

--- javascriptProjects/Shopping List/script.js
+++ javascriptProjects/Shopping List/script.js
@@ -90,6 +90,9 @@ function onClickItem(e) {
 
 function setItemToEdit(item) {
   isEditMode = true;
+  itemList.querySelectorAll("li").forEach((i) => {
+    i.classList.remove("edit-mode");
+  });
   item.classList.add("edit-mode");
   formBtn.innerHTML = '<i class="fa-solid fa-pen"></i> Update Item';
   formBtn.style.backgroundColor = "#228B22";
