d1a9a002b21044d4c10abb8e65dc0c86ed8d171b
itssumitshukla <itssumitshukla@gmail.com>
2026-02-18 15:03:59 -1000

    update function to update item select

--- javascriptProjects/Shopping List/script.js
+++ javascriptProjects/Shopping List/script.js
@@ -92,6 +92,8 @@ function setItemToEdit(item) {
   isEditMode = true;
   item.classList.add("edit-mode");
   formBtn.innerHTML = '<i class="fa-solid fa-pen"></i> Update Item';
+  formBtn.style.backgroundColor = "#228B22";
+  itemInput.value = item.textContent;
 }
 
 function removeItem(item) {
