fbb30717670c7b081c079384d44ed86fb6e91e9e
itssumitshukla <itssumitshukla@gmail.com>
2026-02-25 12:55:42 -1000

    setup movie detail function

--- javascriptProjects/Flixx/js/script.js
+++ javascriptProjects/Flixx/js/script.js
@@ -74,6 +74,71 @@ async function displayPopularShows() {
   });
 }
 
+//display movie details
+async function displayMovieDetails() {
+  const movieId = window.location.search.split("=")[1];
+
+  const movie = await fetchAPIData(`movie/${movieId}`);
+  div.innerHTML = `<div class="details-top">
+  <div>
+  ${
+    movie.poster_path
+      ? `<img
+    src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+    class="card-img-top"
+    alt="${movie.title}"
+  />`
+      : `<img
+  src="../images/no-image.jpg"
+  class="card-img-top"
+  alt="${movie.title}"
+/>`
+  }
+  </div>
+  <div>
+    <h2>${movie.title}</h2>
+    <p>
+      <i class="fas fa-star text-primary"></i>
+      ${movie.vote_average.toFixed(1)} / 10
+    </p>
+    <p class="text-muted">Release Date: ${movie.release_date}</p>
+    <p>
+      ${movie.overview}
+    </p>
+    <h5>Genres</h5>
+    <ul class="list-group">
+      ${movie.genres.map((genre) => `<li>${genre.name}</li>`).join("")}
+    </ul>
+    <a href="${
+      movie.homepage
+    }" target="_blank" class="btn">Visit Movie Homepage</a>
+  </div>
+</div>
+<div class="details-bottom">
+  <h2>Movie Info</h2>
+  <ul>
+    <li><span class="text-secondary">Budget:</span> $${addCommasToNumber(
+      movie.budget,
+    )}</li>
+    <li><span class="text-secondary">Revenue:</span> $${addCommasToNumber(
+      movie.revenue,
+    )}</li>
+    <li><span class="text-secondary">Runtime:</span> ${
+      movie.runtime
+    } minutes</li>
+    <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+  </ul>
+  <h4>Production Companies</h4>
+  <div class="list-group">
+    ${movie.production_companies
+      .map((company) => `<span>${company.name}</span>`)
+      .join(", ")}
+  </div>
+</div>`;
+
+  document.querySelector("#movie-details").appendChild(div);
+}
+
 //Fetch data from TMDB API
 async function fetchAPIData(endpoint) {
   const API_URL = "https://api.themoviedb.org/3/";
@@ -121,6 +186,7 @@ function init() {
       console.log("Shows");
       break;
     case "/movie-details.html":
+      displayMovieDetails();
       console.log("Movie Details");
       break;
     case "/tv-details.html":
