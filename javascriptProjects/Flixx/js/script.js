352423f4b64b65d2212edfbeceb9ddd38ff3096d
itssumitshukla <itssumitshukla@gmail.com>
2026-02-27 14:52:55 -1000

    fix swiper function

--- javascriptProjects/Flixx/js/script.js
+++ javascriptProjects/Flixx/js/script.js
@@ -242,7 +242,22 @@ function displayBackgroundImage(type, backgroundPath) {
 //Show swipper
 async function displaySlider() {
   const { results } = await fetchAPIData("movie/now_playing");
-  console.log(results);
+
+  results.forEach((movie) => {
+    const div = document.createElement("div");
+    div.classList.add("swiper-slide");
+
+    div.innerHTML = `
+      <a href="movie-details.html?id=${movie.id}">
+        <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" />
+      </a>
+      <h4 class="swiper-rating">
+        <i class="fas fa-star text-secondary"></i> ${movie.vote_average} / 10
+      </h4>
+    `;
+
+    document.querySelector(".swiper-wrapper").appendChild(div);
+  });
 }
 
 //Fetch data from TMDB API
