df47cd05032fe07dc5e64473adb773eb21179a73
itssumitshukla <itssumitshukla@gmail.com>
2026-02-27 14:59:44 -1000

    test using swiper cdn

--- javascriptProjects/Flixx/js/script.js
+++ javascriptProjects/Flixx/js/script.js
@@ -257,6 +257,31 @@ async function displaySlider() {
     `;
 
     document.querySelector(".swiper-wrapper").appendChild(div);
+    initSwiper();
+  });
+}
+
+function initSwiper() {
+  const swiper = new Swiper(".swiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    freeMode: true,
+    loop: true,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      500: {
+        slidesPerView: 2,
+      },
+      700: {
+        slidesPerView: 3,
+      },
+      1200: {
+        slidesPerView: 4,
+      },
+    },
   });
 }
 
