9847c5a346721d53fc506bb879e360b66119c8ed
itssumitshukla <itssumitshukla@gmail.com>
2026-03-05 22:04:14 -1000

    setup ctx parameter

--- javascriptProjects/Animated Clock/script.js
+++ javascriptProjects/Animated Clock/script.js
@@ -5,6 +5,8 @@ function clock() {
 
   //Setup canvas
   ctx.save(); // save the default stat
+  ctx.clearRect(0, 0, 500, 500);
+  ctx.translate(250, 250);
 
   ctx.restore(); //Restore default state
 }
