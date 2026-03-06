fe7aee3e758fed07f163e61682bb7eaf5f9b7a82
itssumitshukla <itssumitshukla@gmail.com>
2026-03-05 22:05:51 -1000

    setup ctx default parameter for style

--- javascriptProjects/Animated Clock/script.js
+++ javascriptProjects/Animated Clock/script.js
@@ -7,6 +7,13 @@ function clock() {
   ctx.save(); // save the default stat
   ctx.clearRect(0, 0, 500, 500);
   ctx.translate(250, 250);
+  ctx.rotate(-Math.PI / 2); // Rotate clock -90deg
+
+  // Set default styles
+  ctx.strokeStyle = "#000000";
+  ctx.fillStyle = "#f4f4f4";
+  ctx.lineWidth = 5;
+  ctx.lineCap = "round";
 
   ctx.restore(); //Restore default state
 }
