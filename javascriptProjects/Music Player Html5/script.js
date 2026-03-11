ab7b5015fe66a70ef576176823e87b5467e9bcaa
itssumitshukla <itssumitshukla@gmail.com>
2026-03-10 16:14:10 -1000

    fixprogress bar

--- javascriptProjects/Music Player Html5/script.js
+++ javascriptProjects/Music Player Html5/script.js
@@ -25,16 +25,16 @@ function loadSong(song) {
 
 function playSong() {
   musicContainer.classList.add("play");
-  playBtn.querySelector("i.fas").classList.add("fa-play");
-  playBtn.querySelector("i.fas").classList.remove("fa-pause");
-  audio.pause();
+  playBtn.querySelector("i.fas").classList.remove("fa-play");
+  playBtn.querySelector("i.fas").classList.add("fa-pause");
+  audio.play();
 }
 
 function pauseSong() {
   musicContainer.classList.remove("play");
-  playBtn.querySelector("i.fas").classList.remove("fa-pause");
   playBtn.querySelector("i.fas").classList.add("fa-play");
-  audio.play();
+  playBtn.querySelector("i.fas").classList.remove("fa-pause");
+  audio.pause();
 }
 
 function prevSong() {
@@ -59,6 +59,20 @@ function nextSong() {
   loadSong(songs[songIndex]);
 
   playSong();
+}
+
+function updateProgress(e) {
+  const { duration, currentTime } = e.srcElement;
+  const progressPercent = (currentTime / duration) * 100;
+  progress.style.width = `${progressPercent}%`;
+}
+
+function setProgress(e) {
+  const width = this.clientWidth;
+  const clickX = e.offsetX;
+  const duration = audio.duration;
+
+  audio.currentTime = (clickX / width) * duration;
 }
 
 playBtn.addEventListener("click", () => {
