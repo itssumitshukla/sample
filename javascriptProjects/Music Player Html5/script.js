25f23fa5d4d3c9c22342c887788469e5dc597d62
itssumitshukla <itssumitshukla@gmail.com>
2026-03-10 15:21:45 -1000

    setup pause function

--- javascriptProjects/Music Player Html5/script.js
+++ javascriptProjects/Music Player Html5/script.js
@@ -25,8 +25,15 @@ function loadSong(song) {
 
 function playSong() {
   musicContainer.classList.add("play");
-  playBtn.querySelector("i.fas").classList.remove("fa-play");
-  playBtn.querySelector("i.fas").classList.add("fa-pause");
+  playBtn.querySelector("i.fas").classList.add("fa-play");
+  playBtn.querySelector("i.fas").classList.remove("fa-pause");
+  audio.pause();
+}
+
+function pauseSong() {
+  musicContainer.classList.remove("play");
+  playBtn.querySelector("i.fas").classList.remove("fa-pause");
+  playBtn.querySelector("i.fas").classList.add("fa-play");
   audio.play();
 }
 
