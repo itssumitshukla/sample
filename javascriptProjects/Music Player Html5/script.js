857e28b025967a06c4e2b73f2ca148fba4d29216
itssumitshukla <itssumitshukla@gmail.com>
2026-03-18 22:03:01 -1000

    revert it back to mp3

--- javascriptProjects/Music Player Html5/script.js
+++ javascriptProjects/Music Player Html5/script.js
@@ -19,7 +19,7 @@ loadSong(songs[songIndex]);
 
 function loadSong(song) {
   title.innerText = song;
-  audio.src = `music/${song}.mp4`;
+  audio.src = `music/${song}.mp3`;
   cover.src = `images/${song}.jpg`;
 }
 
