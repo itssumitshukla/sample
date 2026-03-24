943e03c6236d1f9702e789c0ec8acb7a08039e75
itssumitshukla <itssumitshukla@gmail.com>
2026-03-23 17:32:03 -1000

    setup calorie tracker constructor with custom methods

--- javascriptProjects/tracalorie/js/app.js
+++ javascriptProjects/tracalorie/js/app.js
@@ -1,1 +1,18 @@
-class CalorieTracker {}
+class CalorieTracker {
+  constructor() {
+    this._calorieLimit = 2000;
+    this._totalCalories = 0;
+    this._meals = [];
+    this._workouts = [];
+  }
+
+  addMeal(meal) {
+    this._meals.push(meal);
+    this._totalCalories += meal.calories;
+  }
+
+  addWorkout(workout) {
+    this.__workouts.push(workout);
+    this._totalCalories -= workout.calories;
+  }
+}
