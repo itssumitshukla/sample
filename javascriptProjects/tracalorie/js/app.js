220c5e60a42ee9d070d9a9bc9261c75586e77726
itssumitshukla <itssumitshukla@gmail.com>
2026-03-25 23:59:10 -1000

    testing new lunch constructor

--- javascriptProjects/tracalorie/js/app.js
+++ javascriptProjects/tracalorie/js/app.js
@@ -41,8 +41,5 @@ tracker.addMeal(breakfast);
 const run = new Workout("Morning Run", 300);
 tracker.addWorkout(run);
 
-const breakfast1 = new Meal("Breakfast1", 400);
-tracker.addMeal(breakfast);
-
-const run1 = new Workout("Morning Run1", 300);
-tracker.addWorkout(run);
+const lunch = new Meal("Lunch", 2000);
+tracker.addMeal(lunch);
