1db223ef1acd9ebd3b6df26e55f760ccea802b11
itssumitshukla <itssumitshukla@gmail.com>
2026-04-04 12:00:43 -1000

    test workout obj tracker

--- javascriptProjects/tracalorie/js/app.js
+++ javascriptProjects/tracalorie/js/app.js
@@ -133,6 +133,10 @@ class App {
       alert("Please fill in all fields");
       return;
     }
+
+    const workout = new Workout(name.value, +calories.value);
+    this._tracker.addWorkout(workout);
+
     const meal = new Meal(name.value, +calories.value);
     this._tracker.addMeal(meal);
     name.value = "";
