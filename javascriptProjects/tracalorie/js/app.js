e5a9ad008a60d882655c7225b0a1eef7b4c4e338
itssumitshukla <itssumitshukla@gmail.com>
2026-04-05 19:52:15 -1000

    update html and add card body create component

--- javascriptProjects/tracalorie/js/app.js
+++ javascriptProjects/tracalorie/js/app.js
@@ -18,6 +18,7 @@ class CalorieTracker {
   addMeal(meal) {
     this._meals.push(meal);
     this._totalCalories += meal.calories;
+    this._displayNewMeal(meal);
     this._render();
   }
 
@@ -87,6 +88,28 @@ class CalorieTracker {
 
     const width = Math.min(percentage, 100);
     progressEl.style.width = `${width}%`;
+  }
+
+  _displayNewMeal(meal) {
+    const mealsEl = document.getElementById("meal-items");
+    const mealEl = document.createElement("div");
+    mealEl.classList.add("card", "my-2");
+    mealEl.setAttribute("data-id", meal.id);
+    mealEl.innerHTML = `
+                  <div class="card-body">
+                <div class="d-flex align-items-center justify-content-between">
+                  <h4 class="mx-1">Breakfast</h4>
+                  <div
+                    class="fs-1 bg-primary text-white text-center rounded-2 px-2 px-sm-5"
+                  >
+                    350
+                  </div>
+                  <button class="delete btn btn-danger btn-sm mx-2">
+                    <i class="fa-solid fa-xmark"></i>
+                  </button>
+                </div>
+              </div>
+    `;
   }
 
   _render() {
