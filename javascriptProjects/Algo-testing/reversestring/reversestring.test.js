bb8b85cc01896d8e1e524db43f9a4b5265ba1d13
itssumitshukla <itssumitshukla@gmail.com>
2026-04-28 13:03:35 -1000

    setup reverse string test param

--- javascriptProjects/Algo-testing/reversestring/reversestring.test.js
+++ javascriptProjects/Algo-testing/reversestring/reversestring.test.js
@@ -1,3 +1,10 @@
 const reverseString = require("./reversestring");
 
-describe("Reverse String", () => {});
+describe("Reverse String", () => {
+  it("should be a function", () => {
+    expect(typeof reverseString).toEqual("function");
+  });
+  it("should return a string", () => {
+    expect(typeof reverseString("hello")).toEqual("string");
+  });
+});
