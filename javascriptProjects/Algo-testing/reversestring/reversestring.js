36851c35d80ab508a9c250745c17ff783fd27e7c
itssumitshukla <itssumitshukla@gmail.com>
2026-04-28 13:07:35 -1000

    setup solution 2 for reverse str using loop

--- javascriptProjects/Algo-testing/reversestring/reversestring.js
+++ javascriptProjects/Algo-testing/reversestring/reversestring.js
@@ -1,5 +1,14 @@
+//Solution 1
+// function reverseString(str) {
+//   return str.split("").reverse().join("");
+// }
+
+//Solution 2
 function reverseString(str) {
-  return str.split("").reverse().join("");
+  let reversed = "";
+  for (let char of str) {
+    reversed = char + reversed;
+  }
 }
 
 module.exports = reverseString;
