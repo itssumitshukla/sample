fd147ffd5f5c22c7c07206f7bb1350cd5d252cf3
itssumitshukla <itssumitshukla@gmail.com>
2026-04-28 12:54:14 -1000

    setup buzz parameter

--- javascriptProjects/Algo-testing/fizzBuzz/fizzbuzz.test.js
+++ javascriptProjects/Algo-testing/fizzBuzz/fizzbuzz.test.js
@@ -9,9 +9,9 @@ describe("fizzbuzz", () => {
     expect(fizzBuzz(13)).toEqual(13);
     expect(fizzBuzz(17)).toEqual(17);
   });
-  it("should return fizz if divisible by 3", () => {
-    expect(fizzBuzz(3)).toEqual("Fizz");
-    expect(fizzBuzz(6)).toEqual("Fizz");
-    expect(fizzBuzz(12)).toEqual("Fizz");
+  it("should return Buzz if divisible by 5", () => {
+    expect(fizzBuzz(5)).toEqual("Buzz");
+    expect(fizzBuzz(10)).toEqual("Buzz");
+    expect(fizzBuzz(20)).toEqual("Buzz");
   });
 });
