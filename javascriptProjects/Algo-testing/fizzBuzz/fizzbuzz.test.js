b04020ee4177fc267be462e184174c9524bd3ecf
itssumitshukla <itssumitshukla@gmail.com>
2026-04-28 12:53:07 -1000

    setup test parameter for sample test

--- javascriptProjects/Algo-testing/fizzBuzz/fizzbuzz.test.js
+++ javascriptProjects/Algo-testing/fizzBuzz/fizzbuzz.test.js
@@ -4,4 +4,14 @@ describe("fizzbuzz", () => {
   it("should be a function", () => {
     expect(typeof fizzBuzz).toEqual("function");
   });
+  it("should return a number if not divisible by 3 or 5", () => {
+    expect(fizzBuzz(1)).toEqual(1);
+    expect(fizzBuzz(13)).toEqual(13);
+    expect(fizzBuzz(17)).toEqual(17);
+  });
+  it("should return fizz if divisible by 3", () => {
+    expect(fizzBuzz(3)).toEqual("Fizz");
+    expect(fizzBuzz(6)).toEqual("Fizz");
+    expect(fizzBuzz(12)).toEqual("Fizz");
+  });
 });
