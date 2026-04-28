e02709ca8c7a192f5686694383d6eaaa774a74fb
itssumitshukla <itssumitshukla@gmail.com>
2026-04-28 13:06:12 -1000

    setup reverse string basic solve

--- javascriptProjects/Algo-testing/reversestring/reversestring.test.js
+++ javascriptProjects/Algo-testing/reversestring/reversestring.test.js
@@ -7,4 +7,10 @@ describe("Reverse String", () => {
   it("should return a string", () => {
     expect(typeof reverseString("hello")).toEqual("string");
   });
+  it("it should return a reversed string", () => {
+    expect(reverseString("hello")).toEqual("olleh");
+    expect(reverseString("bye")).toEqual("eyb");
+    expect(reverseString("wow")).toEqual("wow");
+    expect(reverseString("hello world")).toEqual("dlrow olleh");
+  });
 });
