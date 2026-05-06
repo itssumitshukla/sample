ce577c62df43a286e8849ffe7d38c0c53ca81f9b
itssumitshukla <itssumitshukla@gmail.com>
2026-05-05 23:10:12 -1000

    search for child nodes

--- javascriptProjects/Algo-testing/getelementsbytag/getelementsbytag.js
+++ javascriptProjects/Algo-testing/getelementsbytag/getelementsbytag.js
@@ -3,6 +3,14 @@ function getElementsByTag(root, tagName) {
   if (!tagName) return [root];
 
   let result = [];
+  //If result is a tag we are looking for
   if (root.tagName.toLowerCase() === tagName.toLowerCase()) {
+    result.push(root);
+  }
+
+  if (root.hasChildNodes()) {
+    for (let child of root.children) {
+      result = result.concat(getElementsByTag());
+    }
   }
 }
