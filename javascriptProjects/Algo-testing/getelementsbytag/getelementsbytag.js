cbcaefb245a2267beb64d6e2c8ce467e9ef9dc7f
itssumitshukla <itssumitshukla@gmail.com>
2026-05-05 23:08:44 -1000

    setup custom function

---
+++ javascriptProjects/Algo-testing/getelementsbytag/getelementsbytag.js
@@ -0,0 +1,8 @@
+function getElementsByTag(root, tagName) {
+  if (!root) return [];
+  if (!tagName) return [root];
+
+  let result = [];
+  if (root.tagName.toLowerCase() === tagName.toLowerCase()) {
+  }
+}
