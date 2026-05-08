25cdd1ece16ddfa43a566814b9649343a6c5cd94
itssumitshukla <itssumitshukla@gmail.com>
2026-05-07 22:21:39 -1000

    setup test params

--- javascriptProjects/Algo-testing/hasduplicateids/hasduplicateids.test.js
+++ javascriptProjects/Algo-testing/hasduplicateids/hasduplicateids.test.js
@@ -1,1 +1,10 @@
 const hasDuplicateIds = require("./hasduplicateids");
+
+describe("DOM Tree has Duplicate IDs", () => {
+  it("Should be a function", () => {
+    expect(typeof hasDuplicateIds).toEqual("function");
+  });
+  it("Should be return a boolean", () => {
+    expect(typeof hasDuplicateIds()).toEqual("boolean");
+  });
+});
