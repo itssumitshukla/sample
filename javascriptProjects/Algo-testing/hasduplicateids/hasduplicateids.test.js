a7e2f15bf177064842cb332d6baf02db23b671b1
itssumitshukla <itssumitshukla@gmail.com>
2026-05-07 22:26:58 -1000

    setup true test case

--- javascriptProjects/Algo-testing/hasduplicateids/hasduplicateids.test.js
+++ javascriptProjects/Algo-testing/hasduplicateids/hasduplicateids.test.js
@@ -7,4 +7,13 @@ describe("DOM Tree has Duplicate IDs", () => {
   it("Should be return a boolean", () => {
     expect(typeof hasDuplicateIds()).toEqual("boolean");
   });
+  it("Should return false if there is no root", () => {
+    expect(typeof hasDuplicateIds()).toBeFalsy();
+  });
+  it("Should return true if there are duplicate ids", () => {
+    //Create mock elements
+    const root = document.createElement("div");
+    const child1 = document.createElement("div");
+    const child2 = document.createElement("div");
+  });
 });
