bef429b8ca594225f9102928c204f138f00c3e98
itssumitshukla <itssumitshukla@gmail.com>
2026-05-31 21:15:52 -1000

    setup middleware

--- javascriptProjects/randomIdeasApp/server.js
+++ javascriptProjects/randomIdeasApp/server.js
@@ -3,6 +3,14 @@ const port = 5000;
 
 const app = express();
 
+//Body Parser Middleware
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: false,
+  }),
+);
+
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to the RandomIdeas API" });
 });
