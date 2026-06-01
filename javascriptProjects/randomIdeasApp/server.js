8e1071d9ced21e37a86bbace029aade8ab43ae33
itssumitshukla <itssumitshukla@gmail.com>
2026-05-31 19:44:31 -1000

    fix npm error

--- javascriptProjects/randomIdeasApp/server.js
+++ javascriptProjects/randomIdeasApp/server.js
@@ -1,15 +1,7 @@
 const express = require("express");
-require("dotenv").config();
-const port = process.env.PORT || 5000;
-const connectDB = require("./config/db");
-
-connectDB();
+const port = 5000;
 
 const app = express();
-
-// Body parser middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to the RandomIdeas API" });
