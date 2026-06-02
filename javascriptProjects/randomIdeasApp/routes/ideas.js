4daaa9afba0039659e68e21c9131c702cf48e3e1
itssumitshukla <itssumitshukla@gmail.com>
2026-06-01 14:33:08 -1000

    setup update idea endpoint

--- javascriptProjects/randomIdeasApp/routes/ideas.js
+++ javascriptProjects/randomIdeasApp/routes/ideas.js
@@ -61,4 +61,20 @@ router.post("/", (req, res) => {
   res.send(req.body.text);
 });
 
+//Update idea
+router.put("/:id", (req, res) => {
+  const idea = ideas.find((idea) => idea.id === +req.params.id);
+
+  if (!idea) {
+    return res
+      .status(404)
+      .json({ success: false, error: "Resource not found" });
+  }
+
+  idea.text = req.body.text || idea.text;
+  idea.tag = req.body.tag || idea.tag;
+
+  res.json({ success: true, data: idea });
+});
+
 module.exports = router;
