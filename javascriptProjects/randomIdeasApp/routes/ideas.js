75afa89a322fa80e0590aafeddbcf1a6cbcda2ed
itssumitshukla <itssumitshukla@gmail.com>
2026-06-01 14:35:55 -1000

    setup delete route

--- javascriptProjects/randomIdeasApp/routes/ideas.js
+++ javascriptProjects/randomIdeasApp/routes/ideas.js
@@ -77,4 +77,20 @@ router.put("/:id", (req, res) => {
   res.json({ success: true, data: idea });
 });
 
+//Delete idea
+router.delete("/:id", (req, res) => {
+  const idea = ideas.find((idea) => idea.id === +req.params.id);
+
+  if (!idea) {
+    return res
+      .status(404)
+      .json({ success: false, error: "Resource not found" });
+  }
+
+  const index = ideas.indexOf(idea);
+  ideas.splice(index, 1);
+
+  res.json({ success: true, data: {} });
+});
+
 module.exports = router;
