79008ce146f301fe40757dddabb08c6989b4addb
itssumitshukla <itssumitshukla@gmail.com>
2026-06-03 18:12:34 -1000

    setup sound database

---
+++ javascriptProjects/Ambient Sound Mixer/js/soundData.js
@@ -0,0 +1,10 @@
+export const sounds = [
+  {
+    id: "rain",
+    name: "Rain",
+    icon: "fa-cloud-rain",
+    color: "from-blue-500 to-cyan-500",
+    file: "rain.mp3",
+    description: "Gentle rainfall",
+  },
+];
