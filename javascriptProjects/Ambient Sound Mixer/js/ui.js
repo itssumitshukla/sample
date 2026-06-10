3ad8a0475c8a85b19909b0fb78a5b44f14456fa5
itssumitshukla <itssumitshukla@gmail.com>
2026-06-10 12:20:50 -1000

    setup create sound card

--- javascriptProjects/Ambient Sound Mixer/js/ui.js
+++ javascriptProjects/Ambient Sound Mixer/js/ui.js
@@ -13,15 +13,23 @@ export class UI {
   }
 
   init() {
-    this.soundCardsContainer = null;
-    this.masterVolumeSlider = null;
-    this.masterVolumeValue = null;
-    this.playPauseButton = null;
-    this.resetButton = null;
-    this.modal = null;
-    this.customPresetContainer = null;
-    this.timerDisplay = null;
-    this.timerSelect = null;
-    this.themeToggle = null;
+    this.soundCardsContainer = document.querySelector(".grid");
+    this.masterVolumeSlider = document.getElementById("masterVolume");
+    this.masterVolumeValue = document.getElementById("masterVolumeValue");
+    this.playPauseButton = document.getElementById("playPauseAll");
+    this.resetButton = document.getElementById("resetAll");
+    this.modal = document.getElementById("savePresetModal");
+    this.customPresetContainer = document.getElementById("customPresets");
+    this.timerDisplay = document.getElementById("timerDisplay");
+    this.timerSelect = document.getElementById("timerSelect");
+    this.themeToggle = document.getElementById("themeToggle");
+  }
+
+  //Create Sound card HTML
+  createSoundCards(sound) {
+    const card = document.createElement("div");
+    card.className =
+      "sound-card bg-whte/10 backdrop-blur-md rounded-2xml p-6 relative overflow-hidden transition-all duration-300";
+    card.dataset.sound = sound.id;
   }
 }
