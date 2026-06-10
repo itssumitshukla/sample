3c0593108aee56946c5f9d2790b4f5098f309909
itssumitshukla <itssumitshukla@gmail.com>
2026-06-10 12:04:29 -1000

    setup UI class constructor

---
+++ javascriptProjects/Ambient Sound Mixer/js/ui.js
@@ -0,0 +1,27 @@
+export class UI {
+  constructor() {
+    this.soundCardsContainer = null;
+    this.masterVolumeSlider = null;
+    this.masterVolumeValue = null;
+    this.playPauseButton = null;
+    this.resetButton = null;
+    this.modal = null;
+    this.customPresetContainer = null;
+    this.timerDisplay = null;
+    this.timerSelect = null;
+    this.themeToggle = null;
+  }
+
+  init() {
+    this.soundCardsContainer = null;
+    this.masterVolumeSlider = null;
+    this.masterVolumeValue = null;
+    this.playPauseButton = null;
+    this.resetButton = null;
+    this.modal = null;
+    this.customPresetContainer = null;
+    this.timerDisplay = null;
+    this.timerSelect = null;
+    this.themeToggle = null;
+  }
+}
