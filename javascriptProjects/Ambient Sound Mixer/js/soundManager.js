983d95a62684a65fe046acc943e122b7b6ff2482
itssumitshukla <itssumitshukla@gmail.com>
2026-07-13 21:49:46 -1000

    setup stop all sound class

--- javascriptProjects/Ambient Sound Mixer/js/soundManager.js
+++ javascriptProjects/Ambient Sound Mixer/js/soundManager.js
@@ -76,4 +76,15 @@ export class SoundManager {
     }
     this.isPlaying = false;
   }
+
+  //Stop all sounds
+  stopAll() {
+    for (const [soundId, audio] of this.audioElements) {
+      if (!audio.paused) {
+        audio.pause();
+      }
+      audio.currentTime = 0;
+    }
+    this.isPlaying = false;
+  }
 }
