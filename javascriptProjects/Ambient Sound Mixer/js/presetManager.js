70300df979194fc7719972153ebe313824adfa4b
itssumitshukla <itssumitshukla@gmail.com>
2026-07-20 22:17:00 -1000

    setup custom preset to look into local storage

--- javascriptProjects/Ambient Sound Mixer/js/presetManager.js
+++ javascriptProjects/Ambient Sound Mixer/js/presetManager.js
@@ -2,4 +2,12 @@ export class PresetManger {
   constructor() {
     this.customPresets = this.loadCustomPresets();
   }
+
+  //Load Presets from local storage
+  loadCustomPresets() {
+    const stored = localStorage.getItem("ambientMixerPresets");
+    return stored ? JSON.parse(stored) : {};
+  }
+
+  //save custom presets to global storage
 }
