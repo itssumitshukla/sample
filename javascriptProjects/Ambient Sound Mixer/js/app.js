4f507a8bd5539e92fca95585ed7412e7fff5bcbb
itssumitshukla <itssumitshukla@gmail.com>
2026-07-28 12:51:39 -1000

    setup hide modal if user clicks on backdrop

--- javascriptProjects/Ambient Sound Mixer/js/app.js
+++ javascriptProjects/Ambient Sound Mixer/js/app.js
@@ -100,6 +100,15 @@ class AmbientMixer {
         this.ui.hideModal();
       });
     }
+
+    //Close modal if backdrop is clicked
+    if (this.ui.modal) {
+      this.ui.modal.addEventListener((e) => {
+        if (e.target === this.ui.modal) {
+          this.ui.hideModal();
+        }
+      });
+    }
   }
 
   //Load all sound
