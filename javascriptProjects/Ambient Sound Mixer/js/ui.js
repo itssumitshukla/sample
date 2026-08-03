4fb134a0c9a605405011e086f405ad52a2e6aac4
itssumitshukla <itssumitshukla@gmail.com>
2026-08-03 11:54:17 -1000

    setup delete preset for UI

--- javascriptProjects/Ambient Sound Mixer/js/ui.js
+++ javascriptProjects/Ambient Sound Mixer/js/ui.js
@@ -215,4 +215,14 @@ export class UI {
       activeButton.classList.add("preset-active");
     }
   }
+
+  //Remove custom preset from UI
+  removeCustomPreset(presetId) {
+    const button = document.querySelector(
+      `.custom-preset-btn[data-preset ="${presetId}"]`,
+    );
+    if (button) {
+      button.remove();
+    }
+  }
 }
