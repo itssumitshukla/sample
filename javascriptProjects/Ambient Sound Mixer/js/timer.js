6985573ddbaddfb27c48c9bb30160ff300a0e6aa
itssumitshukla <itssumitshukla@gmail.com>
2026-08-03 12:43:26 -1000

    setup start interval class

--- javascriptProjects/Ambient Sound Mixer/js/timer.js
+++ javascriptProjects/Ambient Sound Mixer/js/timer.js
@@ -7,4 +7,32 @@ export class Timer {
     this.onTick = onTick;
     this.isRunning = false;
   }
+  //Start timer with duration in minutes
+  start(minutes) {
+    if (minutes <= 0) {
+      this.stop();
+      return;
+    }
+
+    this.duration = minutes * 60; //convert to seconds
+    this.remaining = this.duration;
+    this.isRunning = true;
+
+    //Clear any existing interval
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+
+    //Update Display
+    this.updateDisplay();
+
+    //Start countdown
+    this.intervalId = setInterval(() => {
+      this.remaining--;
+      this.updateDisplay();
+      if (this.remaining <= 0) {
+        this.onComplete();
+      }
+    }, 1000);
+  }
 }
