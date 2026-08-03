a89fbcae02c3eabefccf0c90ea725694882b8ece
itssumitshukla <itssumitshukla@gmail.com>
2026-08-03 12:39:30 -1000

    setup timer class

---
+++ javascriptProjects/Ambient Sound Mixer/js/timer.js
@@ -0,0 +1,10 @@
+export class Timer {
+  constructor(onComplete, onTick) {
+    this.duration = 0;
+    this.remaining = 0;
+    this.intervalId = null;
+    this.onComplete = onComplete;
+    this.onTick = onTick;
+    this.isRunning = false;
+  }
+}
